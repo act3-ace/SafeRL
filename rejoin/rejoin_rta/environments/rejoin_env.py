d4249359ab21a0e49da6a159670cd9b3eda5bb16
Umberto Ravaioli <uravaioli@toyon.com>
2020-10-29 09:30:45 -0700

    rejoin updated to include safe distance from lead and configurable rewards

--- rejoin/rejoin_rta/environments/rejoin_env.py
+++ rejoin/rejoin_rta/environments/rejoin_env.py
@@ -22,9 +22,13 @@ class DubinsRejoin(gym.Env):
         self.obs_norm_const = np.array([10000, 10000, 100, 100], dtype=np.float64)
         self.reward_time_decay = -.01
 
-        self.time_elapsed = 0
+        self.death_radius = 25
+        self.rejoin_max_radius = 150
+        self.rejoin_min_radius = 50
 
-        self._init_reward()
+        self.reward_config = config["reward_config"]
+
+        self.reset()
 
     def step(self, action):
         timestep = 1
@@ -32,19 +36,37 @@ class DubinsRejoin(gym.Env):
         self.env_objs['wingman'].step(timestep, action)
         self.env_objs['lead'].step(timestep)
 
+        # check rejoin condition
+        self.rejoin_failed = False
+        self.in_rejoin = self.rejoin_cond()
+        if self.in_rejoin:
+            self.rejoin_time += timestep
+        elif self.rejoin_time > 0:
+            self.rejoin_time = 0
+            self.rejoin_failed = True
+
+        # check success/failure conditions
+        distance =  np.linalg.norm(self.env_objs['wingman'].position - self.env_objs['lead'].position)
+        if distance >= 10000:
+            self.failure = 'failure_distance'
+        if self.time_elapsed > 1000:
+            self.failure = 'failure_timeout'
+        if distance < self.death_radius:
+            self.failure = 'failure_crash'
+
+        if self.rejoin_time > 20:
+            self.success = True
+
         obs = self._generate_obs()
-        reward = self._generate_reward()
+        reward = self._generate_reward(timestep)
         info = self._generate_info()
 
         self.time_elapsed += timestep
 
         # determine if done
         done = False
-        distance =  np.linalg.norm(self.env_objs['wingman'].position - self.env_objs['lead'].position)
-        if distance >= 10000 or distance <= 5 or self.time_elapsed > 1000:
+        if self.success or self.failure:
             done = True
-            if distance <= 5:
-                reward += 100
 
         return  obs, reward, done, info
 
@@ -55,6 +77,14 @@ class DubinsRejoin(gym.Env):
         self.env_objs['lead'].reset(x=1000, y=0, theta=0, velocity=50)
 
         self.time_elapsed = 0
+        self.success = False
+        self.failure = False
+        
+        self.rejoin_time = 0
+        self.in_rejoin = False
+        self.rejoin_failed = False
+        self.total_reward_rejoin = 0
+        self.rejoin_first_time_applied = False
 
         self._init_reward()
         obs = self._generate_obs()
@@ -74,20 +104,43 @@ class DubinsRejoin(gym.Env):
         # normalize observation
         obs = np.divide(obs, self.obs_norm_const)
 
+        obs = np.clip(obs, -1, 1)
+
         return obs
 
     def _init_reward(self):
         self.prev_distance = np.linalg.norm(self.env_objs['wingman'].position - self.env_objs['lead'].position)
 
-    def _generate_reward(self):
+    def _generate_reward(self, timestep):
         cur_distance = np.linalg.norm(self.env_objs['wingman'].position - self.env_objs['lead'].position)
         dist_change = cur_distance - self.prev_distance
 
-        reward = -1*dist_change/100
+        reward = 0
 
         reward += self.reward_time_decay
 
         self.prev_distance = cur_distance
+
+        if self.in_rejoin:
+            reward_rejoin = self.reward_config['rejoin_timestep'] * timestep
+            reward += reward_rejoin
+            self.total_reward_rejoin += reward_rejoin
+
+            if not self.rejoin_first_time_applied:
+                reward += self.reward_config['rejoin_first_time']
+                self.rejoin_first_time_applied = True
+        else:
+            reward_dist = dist_change*self.reward_config['dist_change']
+            reward += reward_dist
+
+            if self.rejoin_failed:
+                reward += -1*1*self.total_reward_rejoin
+                self.total_reward_rejoin = 0
+
+        if self.failure:
+            reward += self.reward_config[self.failure]
+        elif self.success:
+            reward += self.reward_config['success']
 
         return reward
 
@@ -95,7 +148,16 @@ class DubinsRejoin(gym.Env):
 
         info = {
             'wingman': self.env_objs['wingman']._generate_info(),
-            'lead': self.env_objs['lead']._generate_info()
+            'lead': self.env_objs['lead']._generate_info(),
+            'rejoin_time':self.rejoin_time,
         }
 
         return info
+
+    def rejoin_cond(self):
+        distance =  np.linalg.norm(self.env_objs['wingman'].position - self.env_objs['lead'].position)
+
+        if distance > self.rejoin_min_radius and distance < self.rejoin_max_radius:
+            return True
+        else:
+            return False
