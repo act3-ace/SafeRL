1c57a4c8fb75d9d782417a086374d8e0b25af652
Umberto Ravaioli <uravaioli@toyon.com>
2020-12-16 11:42:48 -0800

    callback functions for tensorboard logging of episode outcomes and failure codes

---
+++ rejoin/rejoin_rta/utils/callbacks.py
@@ -0,0 +1,42 @@
+from typing import Dict
+
+from ray.rllib.agents.callbacks import DefaultCallbacks
+from ray.rllib.env import BaseEnv
+from ray.rllib.evaluation import MultiAgentEpisode, RolloutWorker
+from ray.rllib.policy import Policy
+from ray.rllib.policy.sample_batch import SampleBatch
+
+def build_callbacks_caller(callbacks : [] ):
+
+    class CallbacksCaller(DefaultCallbacks):
+        def __init__(self, legacy_callbacks_dict: Dict[str, callable] = None):
+            self.callbacks = callbacks
+            super(CallbacksCaller, self).__init__(legacy_callbacks_dict)
+
+        def on_episode_end(self, *args, **kwargs):
+            for callback in self.callbacks:
+                callback.on_episode_end(*args, **kwargs)
+            super(CallbacksCaller, self).on_episode_end(*args, **kwargs)
+
+    return CallbacksCaller
+    
+
+class EpisodeOutcomeCallback:
+    def on_episode_end(self, *, worker: RolloutWorker, base_env: BaseEnv,
+                       policies: Dict[str, Policy], episode: MultiAgentEpisode,
+                       env_index: int, **kwargs):
+        episode.custom_metrics["outcome/success"] = int(episode.last_info_for()['success'])
+        episode.custom_metrics["outcome/failure"] = int(bool(episode.last_info_for()['failure']))
+
+
+class FailureCodeCallback:
+    def __init__(self, failure_codes = ['timeout', 'distance', 'crash']):
+        self.failure_codes = failure_codes
+
+    def on_episode_end(self, *, worker: RolloutWorker, base_env: BaseEnv,
+                       policies: Dict[str, Policy], episode: MultiAgentEpisode,
+                       env_index: int, **kwargs):
+
+        if episode.last_info_for()['failure']:
+            for failure_code in self.failure_codes:
+                episode.custom_metrics["failure_code_ratio/{}".format(failure_code)] = int(episode.last_info_for()['failure'] == failure_code)
--- rejoin/train.py
+++ rejoin/train.py
@@ -18,10 +18,11 @@ import ray.rllib.agents.ppo as ppo
 from ray.tune.logger import JsonLogger
 
 from rejoin_rta.environments.rejoin_env import DubinsRejoin
+from rejoin_rta.utils.callbacks import build_callbacks_caller, EpisodeOutcomeCallback, FailureCodeCallback
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--output_dir', type=str, default='./output')
+parser.add_argument('--output_dir', type=str, default='./output/debug')
 
 args = parser.parse_args()
 
@@ -67,14 +68,17 @@ config["num_gpus"] = 0
 config["num_workers"] = 6
 config['_fake_gpus'] = True
 config['seed'] = 0
+config['callbacks'] = build_callbacks_caller([EpisodeOutcomeCallback(), FailureCodeCallback()])
 
 rollout_history = []
 
 reward_config = {
     'time_decay': -0.01,
-    'failure_timeout': -1,
-    'failure_crash': -1,
-    'failure_distance': -1,
+    'failure': {
+        'timeout': -1,
+        'crash': -1,
+        'distance': -1,
+    },
     'success': 1,
     'rejoin_timestep': 0.1,
     'rejoin_first_time': 0.25,
@@ -139,4 +143,4 @@ with open(args_yaml_filepath, 'w') as args_yaml_file:
 with open(ray_config_yaml_filepath, 'w') as ray_config_yaml_file:
     yaml.dump(config, ray_config_yaml_file)
 
-tune.run(ppo.PPOTrainer, config=config, stop=stop_dict, local_dir=args.output_dir, checkpoint_freq=2, checkpoint_at_end=True, name=expr_name)
+tune.run(ppo.PPOTrainer, config=config, stop=stop_dict, local_dir=args.output_dir, checkpoint_freq=25, checkpoint_at_end=True, name=expr_name)
