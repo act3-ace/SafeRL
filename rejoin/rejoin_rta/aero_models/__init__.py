286483230df0cc2137f20e57112d50e10dd8604f
Umberto Ravaioli <uravaioli@toyon.com>
2021-01-22 18:24:22 -0800

    controller step replaced with get control and agent controller completed

--- rejoin/rejoin_rta/aero_models/__init__.py
+++ rejoin/rejoin_rta/aero_models/__init__.py
@@ -1,4 +1,5 @@
 import abc
+import copy
 import numpy as np
 from gym.spaces import Discrete, Box, Tuple
 
@@ -38,60 +39,96 @@ class BaseController(abc.ABC):
         self.config = config
 
     @abc.abstractmethod
-    def step(self, step_size, state, action=None):
+    def generate_control(self, state, action=None):
         ...
 
-class DynamicsController(BaseController):
+class PassThroughController(BaseController):
+
+    def generate_control(self, state, action=None):
+        return action
+
+class AgentController(BaseController):
+
     def __init__(self, dynamics, config=None):
         self.dynamics = dynamics
         self.config = config
 
-    def preprocess_action(self, action):
-        return action
-
-    def step(self, step_size, state, action=None):
-        control = self.preprocess_action(action)
-
-        next_state = self.dynamics.step(step_size, state, control)
-
-        return next_state
-
-class AgentController(DynamicsController):
+        self.setup_action_space()
  
-    def setup_action_space(self, actuators):
+    def setup_action_space(self):
+
+        actuators = self.dynamics.actuators
+
         num_actuators = len(actuators)
         action_space_tup = ()
-        self.action_vals = []
-        for actuator in actuators:
+        self.action_preprocessors = []
+        self.action_control_mapping = []
+
+        # loop over dynamics actuators and match with actuator configs
+        for control_idx, actuator in enumerate(actuators):
             if actuator.name not in self.config['actuators']:
-                raise ValueError("Actuator {} not found in action space config".format(actuator.name))
-
-            actuator_config = self.config['actuators'][actuator.name]
-            if actuator_config['space'] == 'discrete':
-                vals = np.linspace(actuator.bounds[0], actuator.bounds[1], actuator_config['points'])
-                actuator_action_space = Discrete(actuator_config['points'])
-
-            elif actuator_config['space'] == 'continuous':
-                vals = None
-                actuator_action_space = Box(low=actuator.bounds[0], high=actuator.bounds[1], shape=(1,1))
+                continue
             else:
-                raise ValueError("Action Config for Actuator {} has invalid space of {}. Should be 'continuous' or 'discrete'")
+                actuator_config = self.config['actuators'][actuator.name]
 
-            action_space_tup += (actuator_action_space, )
-            self.action_vals.append(vals)
+                if actuator.space == 'continuous':
+                    
+                    if actuator_config['space'] == 'discrete':
+                        # if actuator in continuous but config is discrete, discretize actuator bounds
+                        vals = np.linspace(actuator.bounds[0], actuator.bounds[1], actuator_config['points'])
+                        preprocessor = ActionPreprocessorDiscreteMap(vals)
+                        actuator_action_space = Discrete(actuator_config['points'])
+
+                    elif actuator_config['space'] == 'continuous':
+                        # if both actuator and config are continuous, simply pass through value to control
+                        preprocessor = ActionPreprocessorPassThrough
+                        actuator_action_space = Box(low=actuator.bounds[0], high=actuator.bounds[1], shape=(1,1))
+                    else:
+                        raise ValueError("Action Config for Actuator {} has invalid space of {}. Should be 'continuous' or 'discrete'".format(actuator.name, actuator.space))
+
+                elif actuator.space == 'discrete':
+                    # if the actuator is discrete, ignore actuator config. Use actuator defined points and pass through value to control
+                    preprocessor = ActionPreprocessorPassThrough
+                    actuator_action_space = Discrete(actuator.points)
+                else:
+                    raise ValueError("Actuator {} has invalid space of {}. Should be 'continuous' or 'discrete'".format(actuator.name, actuator.space))
+
+                action_space_tup += (actuator_action_space, )
+
+                # create preprocessor function to process action index input to control input
+                self.action_preprocessors.append(preprocessor)
+
+                # map action idx to actuator control idx
+                self.action_control_mapping.append(control_idx)
 
         self.action_space = Tuple(action_space_tup)
 
-    def preprocess_action(self, action):
-        return action # TODO
+    def generate_control(self, state, action=None):
+        control = copy.deepcopy(self.dynamics.default_control)
 
-    def step(self, step_size, state, action, dynamics):
+        if action is not None:
+            for i, action_val in enumerate(action):
+                if action_val is not None:
+                    action_val_processed = self.action_preprocessors[i](action_val)
+                    control[self.action_control_mapping[i]] = action_val_processed
 
-        action = self.preprocess_action(action)
+        return control
 
-        next_state = dynamics.step(state, action)
+class ActionPreprocessor(abc.ABC):
+    @abc.abstractmethod
+    def __call__(self, action):
+        ...
 
-        return next_state
+class ActionPreprocessorPassThrough(ActionPreprocessor):
+    def __call__(self, action):
+        return action
+
+class ActionPreprocessorDiscreteMap(ActionPreprocessor):
+    def __init__(self, vals):
+        self.vals = vals
+
+    def __call__(self, action):
+        return self.vals[action]
 
 
 
