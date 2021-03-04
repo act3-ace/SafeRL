06863a6143672adaea67178917ef4207144661e5
Umberto Ravaioli <uravaioli@toyon.com>
2021-03-03 19:12:27 -0800

    cwh spacecraft migrated to base platform spec

--- rejoin/rejoin_rta/aero_models/__init__.py
+++ rejoin/rejoin_rta/aero_models/__init__.py
@@ -4,19 +4,6 @@ import numpy as np
 from gym.spaces import Discrete, Box, Tuple
 from scipy import integrate
 
-class BaseActuatorManager(abc.ABC):
-
-
-    @property
-    @abc.abstractmethod
-    def default_control(self):
-        ...
-
-    @property
-    @abc.abstractmethod
-    def actuators(self):
-        ...
-
 class BaseActuator(abc.ABC):
 
     @property
@@ -29,11 +16,21 @@ class BaseActuator(abc.ABC):
     def space(self) -> str:
         ...
 
+    @property
+    @abc.abstractmethod
+    def default(self):
+        ...
+
 class ContinuousActuator(BaseActuator):
-    def __init__(self, name, bounds):
+    def __init__(self, name, bounds, default):
         self._name = name
         self._space = 'continuous'
         self._bounds = bounds
+
+        if isinstance(default, np.ndarray):
+            self._default = default
+        else:
+            self._default = np.array(default, ndmin=1, dtype=np.float64)
 
     @property
     def name(self) -> str:
@@ -45,147 +42,195 @@ class ContinuousActuator(BaseActuator):
 
     @property
     def bounds(self) -> list:
-        return self._bounds
+        return copy.deepcopy(self._bounds)
+
+    @property
+    def default(self):
+        return copy.deepcopy(self._default)
 
 class BaseController(abc.ABC):
     def __init__(self, config = None):
         self.config = config
 
     @abc.abstractmethod
-    def generate_control(self, state, action=None):
+    def gen_actuation(self, state, action=None):
         ...
 
 class PassThroughController(BaseController):
 
-    def generate_control(self, state, action=None):
+    def gen_actuation(self, state, action=None):
         return action
 
 class AgentController(BaseController):
 
-    def __init__(self, dynamics, config=None):
-        self.dynamics = dynamics
+    def __init__(self, actuator_set, config):
         self.config = config
+        self.actuator_set = actuator_set
+        self.actuator_config_list = self.config['actuators']
 
         self.setup_action_space()
  
     def setup_action_space(self):
 
-        actuators = self.dynamics.actuators
+        actuators = self.actuator_set.actuators
 
-        num_actuators = len(actuators)
-        action_space_tup = ()
         self.action_preprocessors = []
-        self.action_control_mapping = []
+        action_space_tup = ()
 
-        # loop over dynamics actuators and match with actuator configs
-        for control_idx, actuator in enumerate(actuators):
-            if actuator.name not in self.config['actuators']:
-                continue
+        # loop over actuators in controller config and setup preprocessors
+        for i, actuator_config in enumerate(self.actuator_config_list):
+            actuator_name = actuator_config['name']
+
+            # get associated actuator from platform's actuator_set
+            if actuator_name in self.actuator_set.name_idx_map:
+                actuator = self.actuator_set.actuators[ self.actuator_set.name_idx_map[actuator_name] ]
             else:
-                actuator_config = self.config['actuators'][actuator.name]
+                raise ValueError("Actuator name {} not found in platform's actuator set".format(actuator_name))
 
-                if actuator.space == 'continuous':
-                    # determine upper and lower bounds of actuator range. Should be the intersection of the actuator object bounds and the actuator config bounds
-                    if 'bounds' in actuator_config:
-                        bounds_min = max(actuator.bounds[0], actuator_config['bounds'][0])
-                        bounds_max = min(actuator.bounds[1], actuator_config['bounds'][1])
-                    else:
-                        bounds_min = actuator.bounds[0]
-                        bounds_max = actuator.bounds[1]
-
-                    if actuator_config['space'] == 'discrete':
-                        # if actuator in continuous but config is discrete, discretize actuator bounds
-                        vals = np.linspace(bounds_min, bounds_max, actuator_config['points'])
-                        preprocessor = ActionPreprocessorDiscreteMap(vals)
-                        actuator_action_space = Discrete(actuator_config['points'])
-
-                    elif actuator_config['space'] == 'continuous':
-                        # if both actuator and config are continuous, simply pass through value to control
-                        preprocessor = ActionPreprocessorPassThrough()
-                        actuator_action_space = Box(low=bounds_min, high=bounds_max, shape=(1,))
-                    else:
-                        raise ValueError("Action Config for Actuator {} has invalid space of {}. Should be 'continuous' or 'discrete'".format(actuator.name, actuator.space))
-
-                elif actuator.space == 'discrete':
-                    # if the actuator is discrete, ignore actuator config. Use actuator defined points and pass through value to control
-                    raise NotImplementedError
-                    preprocessor = None
-                    actuator_action_space = Discrete(actuator.points)
+            if actuator.space == 'continuous':
+                # determine upper and lower bounds of actuator range. Should be the intersection of the actuator object bounds and the actuator config bounds
+                if 'bounds' in actuator_config:
+                    bounds_min = max(actuator.bounds[0], actuator_config['bounds'][0])
+                    bounds_max = min(actuator.bounds[1], actuator_config['bounds'][1])
                 else:
-                    raise ValueError("Actuator {} has invalid space of {}. Should be 'continuous' or 'discrete'".format(actuator.name, actuator.space))
+                    bounds_min = actuator.bounds[0]
+                    bounds_max = actuator.bounds[1]
 
-                action_space_tup += (actuator_action_space, )
+                if ('space' not in actuator_config) or (actuator_config['space'] == 'continuous'):
+                    # if both actuator and config are continuous, simply pass through value to control
+                    preprocessor = ActionPreprocessorPassThrough(actuator_name)
+                    actuator_action_space = Box(low=bounds_min, high=bounds_max, shape=(1,))
+                elif actuator_config['space'] == 'discrete':
+                    # if actuator in continuous but config is discrete, discretize actuator bounds
+                    vals = np.linspace(bounds_min, bounds_max, actuator_config['points'])
+                    preprocessor = ActionPreprocessorDiscreteMap(actuator_name, vals)
+                    actuator_action_space = Discrete(actuator_config['points'])
+                else:
+                    raise ValueError("Action Config for Actuator {} has invalid space of {}. Should be 'continuous' or 'discrete'".format(actuator.name, actuator_config['space']))
 
-                # create preprocessor function to process action index input to control input
-                self.action_preprocessors.append(preprocessor)
+            elif actuator.space == 'discrete':
+                # if the actuator is discrete, ignore actuator config. Use actuator defined points and pass through value to control
+                raise NotImplementedError
 
-                # map action idx to actuator control idx
-                self.action_control_mapping.append(control_idx)
+            else:
+                raise ValueError("Actuator {} has invalid space of {}. Should be 'continuous' or 'discrete'".format(actuator.name, actuator.space))
+            
+            # append actuator action space and preprocessor
+            action_space_tup += (actuator_action_space, )
+            self.action_preprocessors.append(preprocessor)
+
 
         self.action_space = Tuple(action_space_tup)
 
-    def generate_control(self, state, action=None):
-        control = copy.deepcopy(self.dynamics.default_control)
+    def gen_actuation(self, state, action=None):
+        actuation = {}
 
         if action is not None:
             for i, action_val in enumerate(action):
                 if action_val is not None:
-                    action_val_processed = self.action_preprocessors[i](action_val)
-                    control[self.action_control_mapping[i]] = action_val_processed
 
-        return control
+                    if not isinstance(action_val, np.ndarray):
+                        action_val = np.array(action_val, ndmin=1)
+
+                    actuator_name, action_processed = self.action_preprocessors[i](action_val)
+                    actuation[actuator_name] = action_processed
+            
+        return actuation
 
 class ActionPreprocessor(abc.ABC):
+    def __init__(self, name):
+        self.name = name
+    
     @abc.abstractmethod
-    def __call__(self, action):
+    def preprocess(self, action):
         ...
 
-class ActionPreprocessorPassThrough(ActionPreprocessor):
     def __call__(self, action):
+        return copy.deepcopy(self.name), self.preprocess(action)
+
+class ActionPreprocessorPassThrough(ActionPreprocessor):
+    
+    def preprocess(self, action):
         return action
 
 class ActionPreprocessorDiscreteMap(ActionPreprocessor):
-    def __init__(self, vals):
+   
+   
+    def __init__(self, name, vals):
         self.vals = vals
+        super().__init__(name)
 
-    def __call__(self, action):
+    def preprocess(self, action):
         return self.vals[action]
 
+class BaseActuatorSet:
+    def __init__(self, actuators):
+        self.actuators = actuators
+
+        self.name_idx_map = {}
+        for i, actuator in enumerate(self.actuators):
+            self.name_idx_map[actuator.name] = i
+
+    def gen_control(self, actuation=None):
+        control_list = []
+
+        if actuation is None:
+            actuation = {}
+
+        for actuator in self.actuators:
+            actuator_name = actuator.name
+            
+            if actuator_name in actuation:
+                actuator_control = actuation[actuator_name]
+            else:
+                actuator_control = actuator.default
+
+            control_list.append(actuator_control)
+
+        control = np.concatenate(control_list)
+
+        return control
 
 class BasePlatform(abc.ABC):
 
 
-    def __init__(self, dynamics, actuator_manager, controller, init_dict=None):
+    def __init__(self, dynamics, actuator_set, controller, init_dict={}):
 
         self.dependent_objs = []
 
         self.dynamics = dynamics
-        self.actuator_manager = actuator_manager
+        self.actuator_set = actuator_set
         self.controller = controller
 
-        self.reset(init_dict)
+        self.reset(**init_dict)
 
-    def reset(self, init_dict):
-        self.state = self.init_state(init_dict)
+    def reset(self, **kwargs):
+        self.state = self.init_state(**kwargs)
         self.actuation_cur = None
         self.control_cur = None
+
+        for obj in self.dependent_objs:
+            obj.reset()
 
     def step(self, step_size, action=None):
         actuation = self.controller.gen_actuation(self.state, action)
 
-        control = self.actuator_manager.gen_control(actuation)
+        control = self.actuator_set.gen_control(actuation)
 
         # TODO save current actuation
-        self.control_cur = np.copy(control)
+        self.actuation_cur = copy.deepcopy(actuation)
+        self.control_cur = copy.deepcopy(control)
 
-        self.state = self.compute_step(self.state, control)
+        self.state = self.dynamics.step(step_size, self.state, control)
+
+        for obj in self.dependent_objs:
+            obj.step()
+
+    def register_dependent_obj(self, obj):
+        self.dependent_objs.append(obj)
 
     @abc.abstractmethod
-    def compute_step(self, state, control):
-        ...
-
-    @abc.abstractmethod
-    def init_state(self, init_dict):
+    def init_state(self, **kwargs):
         ...
 
 class BaseDynamics(abc.ABC):
@@ -207,11 +252,11 @@ class BaseODESolverDynmaics(BaseDynamics):
 
     def step(self, step_size, state, control):
 
-        if self.integration_method == "rk45":
+        if self.integration_method == "RK45":
             sol = integrate.solve_ivp(self.dx, (0,step_size), state, args=(control,))
 
             state = sol.y[:,-1] # save last timestep of integration solution 
-        elif self.integration_method == 'euler': # euler
+        elif self.integration_method == 'Euler':
             state_dot = self.dx(0, state, control)
             state = state + step_size * state_dot
         else:
@@ -239,4 +284,4 @@ class BaseLinearODESolverDynamics(BaseODESolverDynmaics):
 
     def step(self, step_size, state, control):
         self.update_dynamics_matrices()
-        super().step(self, step_size, state, control)
+        return super().step(step_size, state, control)
--- rejoin/rejoin_rta/aero_models/cwh_spacecraft.py
+++ rejoin/rejoin_rta/aero_models/cwh_spacecraft.py
@@ -3,64 +3,38 @@ from gym import spaces
 from gym.utils import seeding
 import math
 import numpy as np
-from scipy import integrate
+from scipy.spatial.transform import Rotation
 
-from rejoin_rta.aero_models import ContinuousActuator, PassThroughController, AgentController
+from rejoin_rta.aero_models import ContinuousActuator, PassThroughController, AgentController, BaseActuatorSet, BaseLinearODESolverDynamics, BasePlatform
 
 def test_diff(t,x,a):
     return a
 
-class CWHSpacecraft:
-    def __init__(self, config=None, x=0, y=0, z=0, x_dot=0, y_dot=0, z_dot=0, precision=64):
-        
-        self.dependent_objs = []
+class CWHSpacecraft(BasePlatform):
 
-        self.config = config
 
-        if precision == 32:
-            self.precision_dtype = np.float32
-        elif precision == 64:
-            self.precision_dtype = np.float64
-        else:
-            self.precision_dtype = np.float64
+    def __init__(self, config=None, **kwargs):
+        dynamics = CWH3dDynamics()
+        actuator_set = CWH3dActuatorSet()
 
-        self.dynamics = CWHDynamics()
-        self.include_actuator_info = False
-
-        if self.config is None or 'controller' not in self.config:
+        if config is None or 'controller' not in config:
             controller_config = {
                 'type': 'pass'
             }
         else:
-            controller_config = self.config['controller']
+            controller_config = config['controller']
 
         if controller_config['type'] == 'pass':
             controller = PassThroughController(config=controller_config)
         elif controller_config['type'] == 'agent':
-            controller = AgentController(self.dynamics, config=controller_config)
+            controller = AgentController(actuator_set, config=controller_config)
             self.action_space = controller.action_space
-            self.include_actuator_info = True
 
-        self.controller = controller
+        super().__init__(dynamics, actuator_set, controller, init_dict=kwargs)
 
-        self.reset(x=x, y=y, z=z, x_dot=x_dot, y_dot=y_dot, z_dot=z_dot)
-
-    def reset(self, x=0, y=0, z=0, x_dot=0, y_dot=0, z_dot=0):
+    def init_state(self, x=0, y=0, z=0, x_dot=0, y_dot=0, z_dot=0):
         
-        self.state = np.array([x, y, z, x_dot, y_dot, z_dot], dtype=self.precision_dtype)
-        # self.controller.reset()
-
-        for obj in self.dependent_objs:
-            obj.reset()
-
-    def step(self, step_size, action=None):
-
-        control = self.controller.generate_control(self.state, action)
-
-        self.state = self.dynamics.step(step_size, self.state, control)
-
-    def register_dependent_obj(self, obj):
-        self.dependent_objs.append(obj)
+        return np.array([x, y, z, x_dot, y_dot, z_dot], dtype=np.float64)
 
     def _generate_info(self):
         info = {
@@ -73,13 +47,10 @@ class CWHSpacecraft:
             'z_dot': self.z_dot
         }
 
-        if self.include_actuator_info:
-            info['actuators'] = self.dynamics.get_actuator_info()
-
         return info
 
-    def estimate_trajectory(self, time_window=20, num_points=None):
-        return self.dynamics.estimate_trajectory(self.state, time_window=time_window, num_points=num_points)
+    # def estimate_trajectory(self, time_window=20, num_points=None):
+    #     return self.dynamics.estimate_trajectory(self.state, time_window=time_window, num_points=num_points)
 
     @property
     def x(self):
@@ -121,48 +92,49 @@ class CWHSpacecraft:
     def state2d(self):
         return self.state[ [0, 1, 3 ,4] ]
 
-class CWHDynamics:
-    def __init__(self, precision_dtype = np.float64):
+    @property
+    def orientation(self):
+        # always return a no rotation quaternion as points do not have an orientation
+        return Rotation.from_quat([0, 0, 0, 1])
+
+
+class CWH3dActuatorSet(BaseActuatorSet):
+
+
+    def __init__(self):
+
+        actuators = [
+            ContinuousActuator(
+                'thrust_x',
+                [-100, 100],
+                0
+            ),
+            ContinuousActuator(
+                'thrust_y',
+                [-100, 100],
+                0
+            ),
+            ContinuousActuator(
+                'thrust_z',
+                [-100, 100],
+                0
+            ),
+        ]
+
+        super().__init__(actuators)
+
+class CWH3dDynamics(BaseLinearODESolverDynamics):
+    def __init__(self, integration_method = 'RK45'):
         self.m = 12 # kg
         self.n = 0.001027 # rads/s
 
-        self.integration_method = 'rk45'
-        self.precision_dtype = precision_dtype
+        super().__init__(integration_method=integration_method)
 
-        self.control_cur = None
+    def gen_dynamics_matrices(self):
 
-        self.A, self.B = self.get_dynamics_matrices(self.m, self.n)
+        m = self.m
+        n = self.n
 
-        self.actuators = self.define_actuators()
-        self.default_control = np.array([0,0,0], dtype=np.float64)
-
-    def step(self, step_size, state, control=None):
-
-        if control is None:
-            control = self.default_control
-
-        # save the control action for the current timestep
-        self.control_cur = np.copy(control)
-
-        if self.integration_method == "rk45":
-            sol = integrate.solve_ivp(self.dynamics_dx, (0,step_size), state, args=(control,))
-
-            state = sol.y[:,-1] # save last timestep of integration solution 
-        elif self.integration_method == 'euler': # euler
-            state_dot = self.dynamics_dx(0, state, control)
-            state = state + step_size * state_dot
-        else:
-            raise ValueError("invalid integration method '{}'".format(self.integration_method))
-
-        return state
-
-    def dynamics_dx(self, t, state, control):
-
-        state_dot = np.matmul(self.A, state) + np.matmul(self.B, control)
-
-        return state_dot
-
-    def get_dynamics_matrices(self, m, n):
         A = np.array([
             [0,      0, 0,     1,     0,  0],
             [0,      0, 0,     0,     1,  0],
@@ -170,7 +142,7 @@ class CWHDynamics:
             [3*n**2, 0, 0,     0,    2*n, 0],
             [0,      0, 0,     -2*n, 0,   0],
             [0,      0, -n**2, 0,    0,   0],
-        ], dtype=self.precision_dtype)
+        ], dtype=np.float64)
 
         B = np.array([
             [0,   0,   0   ],
@@ -179,44 +151,6 @@ class CWHDynamics:
             [1/m, 0,   0   ],
             [0,   1/m, 0   ],
             [0,   0,   1/m ],
-        ], dtype=self.precision_dtype)
+        ], dtype=np.float64)
 
         return A, B
-
-    def define_actuators(self):
-        actuators = [
-            ContinuousActuator(
-                'thrust_x',
-                [-100, 100]
-            ),
-            ContinuousActuator(
-                'thrust_y',
-                [-100, 100]
-            ),
-            ContinuousActuator(
-                'thrust_z',
-                [-100, 100]
-            ),
-        ]
-
-        return actuators
-
-    def get_actuator_info(self):
-        if self.control_cur is None:
-            info = None
-        else:
-            info = {}
-
-            for i, actuator in enumerate(self.actuators):
-                info[actuator.name] = self.control_cur[i]
-
-        return info
-
-    def estimate_trajectory(self, state, time_window=20, num_points=None):
-        control = self.default_control
-        if num_points:
-            t_eval = np.linspace(0, time_window, num_points)
-
-        sol = integrate.solve_ivp(self.dynamics_dx, (0,time_window), state, args=(control,), t_eval=t_eval)
-
-        return sol.y[0:3,:]
