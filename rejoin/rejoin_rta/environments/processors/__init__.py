7af21e3833eff87dbefce482f37a38985ac72258
james.cunningham <james.cunningham@jacobs.com>
2021-03-08 14:31:19 -0500

    Modularize and implement reward and status processors

--- rejoin/rejoin_rta/environments/processors/__init__.py
+++ rejoin/rejoin_rta/environments/processors/__init__.py
@@ -4,24 +4,74 @@ from abc import ABC, abstractmethod
 class Processor(ABC):
     def __init__(self, config, name="generic_processor"):
         self.config = config
-        self.step_value = 0
-        self.total_value = 0
         self.name = name
 
+    @abstractmethod
     def reset(self, env_objs):
         """Reset the processor instance"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _generate_info(self) -> dict:
+        """Create and return an info dict"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def step(self, env_objs, timestep, status, old_status):
+        """Process the environment at the current timestep"""
+        raise NotImplementedError
+
+
+class StatusProcessor(Processor):
+    def __init__(self, config, name="status_processor"):
+        super().__init__(config=config, name=name)
+        self.status_value = None
+
+    def reset(self, env_objs):
+        self.status_value = None
+
+    def _generate_info(self) -> dict:
+        info = {
+            "status": self.status_value
+        }
+        return info
+
+    def step(self, env_objs, timestep, status, old_status):
+        self.status_value = self.generate_status(
+            env_objs=env_objs,
+            timestep=timestep,
+            status=status,
+            old_status=old_status
+        )
+        status[self.name] = self.status_value
+
+    @abstractmethod
+    def generate_status(self, env_objs, timestep, status, old_status):
+        """Generate a status value from the environment at the current timestep"""
+        raise NotImplementedError
+
+
+class RewardProcessor(Processor):
+    def __init__(self, config, name="reward_processor"):
+        super().__init__(config=config, name=name)
+        self.step_value = 0
+        self.total_value = 0
+
+    def reset(self, env_objs):
         self.step_value = 0
         self.total_value = 0
 
     def _generate_info(self) -> dict:
-        """Create and return an info dict"""
         info = {
             "step": self.step_value,
             "total": self.total_value
         }
         return info
 
+    def step(self, env_objs, timestep, status, old_status=None):
+        self.step_value = self.generate_reward(env_objs, timestep, status)
+        self.total_value += self.step_value
+
     @abstractmethod
-    def step(self, env_objs, time_step, status_dict):
-        """Generate step reward"""
+    def generate_reward(self, env_objs, timestep, status):
         raise NotImplementedError
