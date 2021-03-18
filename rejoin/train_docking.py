521fa61e29cc9fc60032fca7c68e5877bdc4e08b
Umberto Ravaioli <uravaioli@toyon.com>
2021-03-17 21:41:34 -0400

    Revert "Merge branch 'reward_managers' into 'master'"
    This reverts merge request !6

--- rejoin/train_docking.py
+++ rejoin/train_docking.py
@@ -3,7 +3,7 @@ import gym
 from gym.spaces import Discrete, Box
 import numpy as np
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"]="-1" 
 import yaml
 import math
 
@@ -17,11 +17,9 @@ from ray import tune
 import ray.rllib.agents.ppo as ppo
 from ray.tune.logger import JsonLogger
 
-from rejoin_rta.environments.docking_env import DockingEnv
+from rejoin_rta.environments.docking_env import DockingEnv, DockingObservationProcessor, DockingRewardProcessor, DockingRewardProcessor3D, DockingConstraintProcessor
 from rejoin_rta.utils.callbacks import build_callbacks_caller, EpisodeOutcomeCallback, FailureCodeCallback, RewardComponentsCallback, LoggingCallback
-from rejoin.rejoin_rta.environments.processors.reward_processors import *
-from rejoin.rejoin_rta.environments.processors.status_processors import *
-from rejoin.rejoin_rta.environments.processors.observation_processors import *
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--output_dir', type=str, default='./output')
@@ -50,24 +48,8 @@ config['callbacks'] = build_callbacks_caller([
 
 rollout_history = []
 
-# ------ Define reward configuration ------
-
-reward_processors = [
-    TimeRewardProcessor,
-    DistanceChangeRewardProcessor,
-    FailureRewardProcessor,
-    SuccessRewardProcessor
-]
-
-reward_processors_3d = [
-    TimeRewardProcessor,
-    DistanceChangeZRewardProcessor,
-    FailureRewardProcessor,
-    SuccessRewardProcessor
-]
-
 reward_config = {
-    'processors': reward_processors,
+    'processor': DockingRewardProcessor,
     'time_decay': -0.001,
     'failure': {
         'timeout': -1,
@@ -79,7 +61,7 @@ reward_config = {
 }
 
 reward_config_3d = {
-    'processors': reward_processors_3d,
+    'processor': DockingRewardProcessor3D,
     'time_decay': -0.001,
     'failure': {
         'timeout': -1,
@@ -90,39 +72,6 @@ reward_config_3d = {
     'dist_change': -0.001,
     'dist_z_change': -0.01,
 }
-
-# ------ Define status configuration ------
-
-status_processors = [
-    DockingStatusProcessor,
-    DockingDistanceStatusProcessor,
-    FailureStatusProcessor,
-    SuccessStatusProcessor
-]
-
-status_config = {
-    'processors': status_processors,
-    'timeout': 1000,
-    'max_goal_distance': 40000,
-}
-
-# ------ Define observation configuration ------
-
-observation_processors = [
-    DockingObservationProcessor
-]
-
-observation_config = {
-    'processors': observation_processors,
-    'mode': '2d'
-}
-
-observation_config_3d = {
-    'processors': observation_processors,
-    'mode': '3d'
-}
-
-# ------ Define environment configuration ------
 
 env_config = {
     'reward': reward_config,
@@ -159,13 +108,20 @@ env_config = {
             ]
         },
     },
-    'observation': observation_config,
-    'docking_region': {
+    'obs' : {
+        'processor': DockingObservationProcessor,
+        'mode': '2d'
+    },
+    'docking_region' : {
         'type': 'circle',
         'radius': 20,
     },
-    'status': status_config,
-    'verbose': False,
+    'constraints':{
+        'processor': DockingConstraintProcessor,
+        'timeout': 1000,
+        'max_goal_distance': 40000,
+    },
+    'verbose':False,
 }
 
 env_config3d = {
@@ -213,16 +169,23 @@ env_config3d = {
             ],
         },
     },
-    'observations': observation_config_3d,
-    'docking_region': {
+    'obs' : {
+        'processor': DockingObservationProcessor,
+        'mode': '3d'
+    },
+    'docking_region' : {
         'type': 'cylinder',
         'params': {
             'radius': 300,
             'height': 600,
         }
     },
-    'status': status_config,
-    'verbose': False,
+    'constraints':{
+        'processor': DockingConstraintProcessor,
+        'timeout': 1000,
+        'max_goal_distance': 10000,
+    },
+    'verbose':False,
 }
 
 config['env_config'] = env_config
@@ -232,30 +195,14 @@ stop_dict = {
     'training_iteration': 500,
 }
 
-if __name__ == "__main__":
-    # Workaround for YAML not dumping ABCMeta objects
-    # TODO: See if there is a better way to fix this
-    from yaml.representer import Representer
-    from abc import ABCMeta
-    Representer.add_representer(ABCMeta, Representer.represent_name)
+# create output dir and save experiment params
+os.makedirs(output_dir, exist_ok=True)
+args_yaml_filepath = os.path.join(output_dir, 'script_args.yaml')
+ray_config_yaml_filepath = os.path.join(output_dir, 'ray_config.yaml')
+with open(args_yaml_filepath, 'w') as args_yaml_file:
+    arg_dict = vars(args)
+    yaml.dump(arg_dict, args_yaml_file)
+with open(ray_config_yaml_filepath, 'w') as ray_config_yaml_file:
+    yaml.dump(config, ray_config_yaml_file)
 
-    DEBUG = False
-
-    # create output dir and save experiment params
-    os.makedirs(output_dir, exist_ok=True)
-    args_yaml_filepath = os.path.join(output_dir, 'script_args.yaml')
-    ray_config_yaml_filepath = os.path.join(output_dir, 'ray_config.yaml')
-    with open(args_yaml_filepath, 'w') as args_yaml_file:
-        arg_dict = vars(args)
-        yaml.dump(arg_dict, args_yaml_file)
-    with open(ray_config_yaml_filepath, 'w') as ray_config_yaml_file:
-        yaml.dump(config, ray_config_yaml_file)
-
-    if not DEBUG:
-        tune.run(ppo.PPOTrainer, config=config, stop=stop_dict, local_dir=args.output_dir, checkpoint_freq=25, checkpoint_at_end=True, name=expr_name)
-    else:
-        # Run training in a single process for debugging
-        config["num_workers"] = 0
-        trainer = ppo.PPOTrainer(config=config)
-        while True:
-            print(trainer.train())
+tune.run(ppo.PPOTrainer, config=config, stop=stop_dict, local_dir=args.output_dir, checkpoint_freq=25, checkpoint_at_end=True, name=expr_name)
