404cc8c44291e78b29e0d696994233d5a02ad58f
Umberto Ravaioli <uravaioli@toyon.com>
2021-03-22 18:45:28 -0700

    convert rllib 1.0 model ckpt to keras and matlab

--- rejoin/rejoin_rta/utils/util.py
+++ rejoin/rejoin_rta/utils/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import io
 
 def draw_from_rand_bounds_dict(rand_dict):
     draw_dict = {}
@@ -15,3 +16,20 @@ def draw_from_rand_bounds_dict(rand_dict):
         draw_dict[key] = draw
 
     return draw_dict
+
+def numpy_to_matlab_txt(mat, name=None, output_stream=None):
+    ret_str = False
+    if output_stream is None:
+        output_stream = io.StringIO
+        ret_str = True
+
+    if name:
+        output_stream.write('{} = '.format(name))
+
+    output_stream.write('[\n')
+    np.savetxt(output_stream, mat, delimiter=',', newline=';\n')
+    output_stream.write('];\n')
+
+    if ret_str:
+        return output_stream.getvalue()
+    else: return output_stream    
