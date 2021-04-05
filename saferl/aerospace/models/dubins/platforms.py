99e12043ebb7272a0d76e5a52aeb9b3e1e1515be
james.cunningham <james.cunningham@jacobs.com>
2021-04-05 12:48:45 -0400

    Remove TODO

--- saferl/aerospace/models/dubins/platforms.py
+++ saferl/aerospace/models/dubins/platforms.py
@@ -78,7 +78,6 @@ class BaseDubinsState(BasePlatformStateVectorized):
 class Dubins2dPlatform(BaseDubinsPlatform):
 
     def __init__(self, config=None, controller=None,  **kwargs):
-        # TODO: Set up from config
 
         dynamics = Dubins2dDynamics()
         actuator_set = Dubins2dActuatorSet()
