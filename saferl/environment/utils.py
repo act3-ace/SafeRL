dd03f406d3ca5d2d8e20e7cb918b48dba5c3a565
james.cunningham <james.cunningham@jacobs.com>
2021-04-08 12:29:46 -0400

    Relative paths in config

--- saferl/environment/utils.py
+++ saferl/environment/utils.py
@@ -55,31 +55,32 @@ def setup_env_objs_from_config(config):
 
 def parse_env_config(config_yaml, lookup):
     config_path = os.path.abspath(config_yaml)
+    config_dir = os.path.dirname(config_path)
 
     with open(config_path, 'r') as f:
         config = yaml.load(f)
     env_str = config["env"]
     env_config = config["env_config"]
     env = lookup[env_str]
-    env_config = process_yaml_items(env_config, lookup)
+    env_config = process_yaml_items(env_config, lookup, working_dir=config_dir)
     return env, env_config
 
 
-def process_yaml_items(target, lookup):
+def process_yaml_items(target, lookup, working_dir):
     if isinstance(target, dict):
         for k, v in target.items():
-            target[k] = process_yaml_items(v, lookup)
+            target[k] = process_yaml_items(v, lookup, working_dir=working_dir)
     elif isinstance(target, str):
         if target[0] == PATH_CHAR:
             # Value is path to yaml config
             path_str = target[1:]
-            path = os.path.abspath(path_str)
+            path = os.path.abspath(os.path.join(working_dir, path_str))
             with open(path, 'r') as f:
                 contents = yaml.load(f)
-            target = process_yaml_items(contents, lookup)
+            target = process_yaml_items(contents, lookup, working_dir=os.path.dirname(path))
         elif target in lookup.keys():
             target = lookup[target]
     elif isinstance(target, list):
-        target = [process_yaml_items(i, lookup) for i in target]
+        target = [process_yaml_items(i, lookup, working_dir=working_dir) for i in target]
 
     return target
