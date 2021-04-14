b307667ac9c12bd32b07105c43d80dc06897a788
james.cunningham <james.cunningham@jacobs.com>
2021-04-14 14:19:48 -0400

    Module level lookup dicts

--- saferl/environment/models/__init__.py
+++ saferl/environment/models/__init__.py
@@ -1,2 +1,10 @@
-from .platforms import *
-from .geometry import *
+import saferl.environment.models.platforms
+import saferl.environment.models.geometry
+
+import inspect
+
+platforms_mems = inspect.getmembers(platforms, inspect.isclass)
+geometry_mems = inspect.getmembers(geometry, inspect.isclass)
+mems = platforms_mems + geometry_mems
+
+lookup = {k: v for k, v in mems}
--- saferl/environment/tasks/__init__.py
+++ saferl/environment/tasks/__init__.py
@@ -1,4 +1,14 @@
-from .utils import draw_from_rand_bounds_dict
-from .manager import Manager, RewardManager, StatusManager, ObservationManager
-from .processor import Processor, ObservationProcessor, StatusProcessor, RewardProcessor
-from .env import BaseEnv
+import saferl.environment.tasks.env
+import saferl.environment.tasks.manager
+import saferl.environment.tasks.processor
+import saferl.environment.tasks.utils
+
+import inspect
+
+env_mems = inspect.getmembers(env, inspect.isclass)
+manager_mems = inspect.getmembers(manager, inspect.isclass)
+processor_mems = inspect.getmembers(processor, inspect.isclass)
+utils_mems = inspect.getmembers(utils, inspect.isclass)
+mems = env_mems + manager_mems + utils_mems + processor_mems
+
+lookup = {k: v for k, v in mems}
