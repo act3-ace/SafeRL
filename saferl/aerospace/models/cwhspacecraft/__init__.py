bc8f56caf5094874d4292bea7778e2a0b927e735
Umberto Ravaioli <uravaioli@toyon.com>
2021-04-19 18:50:13 -0700

    potential config lookup dict key fix

--- saferl/aerospace/models/cwhspacecraft/__init__.py
+++ saferl/aerospace/models/cwhspacecraft/__init__.py
@@ -2,4 +2,7 @@ import inspect
 import saferl.aerospace.models.cwhspacecraft.platforms
 
 mems = inspect.getmembers(platforms, inspect.isclass)
-lookup = {v.__module__ + "." + k: v for k, v in mems if "saferl" in str(v)}
+print(mems)
+import pdb; pdb.set_trace()
+lookup = {__name__ + ".platforms." + k: v for k, v in mems if "saferl" in str(v)}
+# lookup = {v.__module__ + "." + k: v for k, v in mems if "saferl" in str(v)}
