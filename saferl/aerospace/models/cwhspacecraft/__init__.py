feb58e2e221664dafb7b0b7e040b5b2d4563202c
Umberto Ravaioli <uravaioli@toyon.com>
2021-04-19 18:51:38 -0700

    removed accidental debugging code

--- saferl/aerospace/models/cwhspacecraft/__init__.py
+++ saferl/aerospace/models/cwhspacecraft/__init__.py
@@ -2,7 +2,6 @@ import inspect
 import saferl.aerospace.models.cwhspacecraft.platforms
 
 mems = inspect.getmembers(platforms, inspect.isclass)
-print(mems)
-import pdb; pdb.set_trace()
+
 lookup = {__name__ + ".platforms." + k: v for k, v in mems if "saferl" in str(v)}
 # lookup = {v.__module__ + "." + k: v for k, v in mems if "saferl" in str(v)}
