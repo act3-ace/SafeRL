9d382199350a99ccc2c04add796ef4172af344e6
Umberto Ravaioli <uravaioli@toyon.com>
2021-04-24 00:26:49 +0000

    removed commented lookup dict construction for cwhspacecraft

--- saferl/aerospace/models/cwhspacecraft/__init__.py
+++ saferl/aerospace/models/cwhspacecraft/__init__.py
@@ -4,4 +4,3 @@ from saferl.aerospace.models.cwhspacecraft import platforms
 mems = inspect.getmembers(platforms, inspect.isclass)
 
 lookup = {__name__ + ".platforms." + k: v for k, v in mems if "saferl" in str(v)}
-# lookup = {v.__module__ + "." + k: v for k, v in mems if "saferl" in str(v)}
