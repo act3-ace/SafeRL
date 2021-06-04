2f3a73ea549bd753b008c108d402fd55a388ea4a
vgangal <Vardaan.Gangal@Jacobs.com>
2021-06-04 11:22:07 -0400

    updated processor names, reflected those changes where necessary, fixed small changes in manual_setup_env.py

--- scripts/manual_env_setup.py
+++ scripts/manual_env_setup.py
@@ -6,8 +6,7 @@ from saferl.environment.utils import YAMLParser
 # use the saferl.environment to set it up
 
 # default config set to docking env
-# CONFIG = '../configs/docking/docking_default.yaml'
-CONFIG = '../configs/docking/docking_default_checkProcessors.yaml'
+CONFIG = '../configs/docking/docking_default.yaml'
 
 # 1. setup an argeparse block to take in config parameters
 parser = argparse.ArgumentParser()
@@ -16,14 +15,14 @@ args = parser.parse_args()
 
 # 2. parse yaml file and call appropriate classes
 parser = YAMLParser(yaml_file=args.config, lookup=lookup)
-env, env_config = parser.parse_env()
+env_class, env_config = parser.parse_env()
 
 # 3 . instantiate the environment class
-envObj = env(env_config)
+setup_env = env_class(env_config)
 
 # 4 - a step on the environment
 
-obs, reward, done, info = envObj.step((0, 0))
+obs, reward, done, info = setup_env.step(None)
 
 print('obs=', obs)
 print('reward=', reward)
