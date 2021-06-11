be86a927735957b39b1b7bbf9aff966ff9cc97a3
Umberto Ravaioli <uravaioli@toyon.com>
2021-06-10 20:47:44 -0700

    update rta module interface

--- saferl/environment/rta/rta.py
+++ saferl/environment/rta/rta.py
@@ -4,12 +4,18 @@ import abc
 class RTAModule(abc.ABC):
 
     def __init__(self):
-        self.rta_on = False
+        self.enable = True
 
     def reset(self):
-        self.rta_on = False
+        self.enable = True
 
     def filter_control(self, sim_state, control):
+        if self.enable:
+            return self._filter_control(sim_state, control)
+        else:
+            return control
+
+    def _filter_control(self, sim_state, control):
         self.monitor(sim_state, control)
         return self.generate_control(sim_state, control)
 
@@ -17,10 +23,7 @@ class RTAModule(abc.ABC):
         self._monitor(sim_state, control)
 
     def generate_control(self, sim_state, control):
-        if self.rta_on:
-            return self._generate_control(sim_state, control)
-        else:
-            return control
+        return self._generate_control(sim_state, control)
 
     @abc.abstractmethod
     def _monitor(self, sim_state, control):
@@ -29,3 +32,21 @@ class RTAModule(abc.ABC):
     @abc.abstractmethod
     def _generate_control(self, sim_state, control):
         raise NotImplementedError()
+
+
+class SimplexModule(RTAModule):
+
+    def __init__(self):
+        self.rta_on = False
+        super().__init__()
+
+    def reset(self):
+        self.rta_on = False
+        super().reset()
+
+    def _filter_control(self, sim_state, control):
+        self.monitor(sim_state, control)
+        if self.rta_on:
+            return self.generate_control(sim_state, control)
+        else:
+            return control
