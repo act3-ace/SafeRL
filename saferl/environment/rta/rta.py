5e9c29d45c8f59bd13fbe19d59c772f520b2c9f5
Umberto Ravaioli <uravaioli@toyon.com>
2021-06-28 17:48:48 -0700

    updated rta interface for simplex

--- saferl/environment/rta/rta.py
+++ saferl/environment/rta/rta.py
@@ -20,38 +20,62 @@ class RTAModule(abc.ABC):
         else:
             return control
 
+    @abc.abstractmethod
     def _filter_control(self, sim_state, step_size, control):
-        self.monitor(sim_state, step_size, control)
-        return self.generate_control(sim_state, step_size, control)
-
-    def monitor(self, sim_state, step_size, control):
-        self._monitor(sim_state, step_size, control)
-
-    def generate_control(self, sim_state, step_size, control):
-        return self._generate_control(sim_state, step_size, control)
-
-    @abc.abstractmethod
-    def _monitor(self, sim_state, step_size, control):
         raise NotImplementedError()
 
-    @abc.abstractmethod
-    def _generate_control(self, sim_state, step_size, control):
-        raise NotImplementedError()
+    def generate_info(self):
+        info = {
+            'enable': self.enable,
+        }
+
+        return info
 
 
 class SimplexModule(RTAModule):
 
     def __init__(self):
-        self.rta_on = False
+        self.backup_on = False
         super().__init__()
 
     def reset(self):
-        self.rta_on = False
+        self.backup_on = False
         super().reset()
 
     def _filter_control(self, sim_state, step_size, control):
-        self.monitor(sim_state, step_size, control)
-        if self.rta_on:
-            return self.generate_control(sim_state, step_size, control)
+        self.backup_on = self.monitor(sim_state, step_size, control)
+
+        if self.backup_on:
+            return self.backup_control(sim_state, step_size, control)
         else:
             return control
+
+    def monitor(self, sim_state, step_size, control):
+        return self._monitor(sim_state, step_size, control)
+
+    def backup_control(self, sim_state, step_size, control):
+        return self._backup_control(sim_state, step_size, control)
+
+    @abc.abstractmethod
+    def _monitor(self, sim_state, step_size, control):
+        '''
+        Returns
+        -------
+        bool
+            True if unsafe
+        '''
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def _backup_control(self, sim_state, step_size, control):
+        raise NotImplementedError()
+
+    def generate_info(self):
+        info = {
+            'backup_on': self.backup_on,
+        }
+
+        info_parent = super().generate_info()
+        info_ret = {**info_parent, **info}
+
+        return info_ret
