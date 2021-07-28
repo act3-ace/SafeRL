ad1a3bc306dcd419a2dc69cbb2a39efecbba2f31
Umberto Ravaioli <uravaioli@toyon.com>
2021-07-27 22:36:59 -0700

    docking initialization simplification and min vel constraint added

--- saferl/aerospace/tasks/docking/initializers.py
+++ saferl/aerospace/tasks/docking/initializers.py
@@ -20,14 +20,12 @@ class ConstrainedDeputyPolarInitializer(Initializer):
         x, y = get_relative_rect_from_polar(ref, radius, angle)
 
         # Get constrained velocity
-        x_dot, y_dot = self.init_config["x_dot"], self.init_config["y_dot"]
-        x_dot, y_dot = get_constrainted_velocity(
-            reference=ref,
-            x=x,
-            y=y,
-            x_dot=x_dot,
-            y_dot=y_dot,
-        )
+        x_dot = self.init_config.get('x_dot', [-10, 10])
+        y_dot = self.init_config.get('y_dot', [-10, 10])
+        enforce_safety_constraints = self.init_config.get('enforce_safety_constraints', True)
+
+        x_dot, y_dot = get_constrainted_velocity(ref, x, y, x_dot, y_dot,
+                                                 enforce_safety_constraints=enforce_safety_constraints)
 
         new_params = {
             "x": x,
@@ -35,92 +33,61 @@ class ConstrainedDeputyPolarInitializer(Initializer):
             "x_dot": x_dot,
             "y_dot": y_dot
         }
-        for k, v in self.init_config.items():
-            if k in new_params:
-                v = new_params[k]
-            new_params[k] = v if type(v) != list else np.random.uniform(v[0], v[1])
+
         return new_params
 
 
-def get_relative_rect_from_polar(reference, radius, angle):
-    ref_x, ref_y = reference.x, reference.y
+def get_relative_rect_from_polar(ref, radius, angle):
+    ref_x, ref_y = ref.x, ref.y
     x = ref_x + radius * np.cos(angle)
     y = ref_y + radius * np.sin(angle)
     return x, y
 
 
-def get_constrainted_velocity(reference, x, y, x_dot, y_dot, mean_motion=0.001027, max_x_dot=10, max_y_dot=10):
-    def set_max_bounds(bounds, max_value):
-        if isinstance(bounds, list):
-            lower, upper = bounds[0], bounds[-1]
-            upper = min(max_value, upper)
-            lower = max(-max_value, lower)
-            bounds[0] = lower
-            bounds[-1] = upper
-        else:
-            bounds = min(bounds, max_value) if bounds > 0 else max(bounds, -max_value)
-        return bounds
-
-    def max_abs_value(bounds):
-        if isinstance(bounds, list):
-            if abs(bounds[0]) >= abs(bounds[-1]):
-                max_abs_bounds = bounds[0]
-                index = 0
-            else:
-                max_abs_bounds = bounds[-1]
-                index = -1
-        else:
-            max_abs_bounds = bounds
-            index = None
-        return max_abs_bounds, index
-
-    def reduce_components(x_dot, y_dot, constraint):
-        max_abs_x_dot, max_abs_x_dot_idx = max_abs_value(x_dot)
-        max_abs_y_dot, max_abs_y_dot_idx = max_abs_value(y_dot)
-        if abs(max_abs_x_dot) > constraint and abs(max_abs_y_dot) > constraint:
-            # Reduce both components
-            new_value = math.sqrt((constraint ** 2) / 2)
-            x_dot_value = new_value if max_abs_x_dot >= 0 else -new_value
-            y_dot_value = new_value if max_abs_y_dot >= 0 else -new_value
-        elif abs(max_abs_x_dot) >= abs(max_abs_y_dot):
-            # abs(x_dot) > abs(y_dot). Reduce x_dot.
-            new_value = math.sqrt(constraint ** 2 - max_abs_y_dot ** 2)
-            x_dot_value = new_value if max_abs_x_dot >= 0 else -new_value
-            y_dot_value = max_abs_y_dot
-        else:
-            # Reduce y_dot
-            new_value = math.sqrt(constraint ** 2 - max_abs_x_dot ** 2)
-            y_dot_value = new_value if max_abs_y_dot >= 0 else -new_value
-            x_dot_value = max_abs_x_dot
-
-        # Assign new values
-        if max_abs_x_dot_idx is not None:
-            x_dot[max_abs_x_dot_idx] = x_dot_value
-        else:
-            x_dot = x_dot_value
-        if max_abs_y_dot_idx is not None:
-            y_dot[max_abs_y_dot_idx] = y_dot_value
-        else:
-            y_dot = y_dot_value
-        return x_dot, y_dot
-
-    rel_x = x - reference.x
-    rel_y = y - reference.y
+def get_constrainted_velocity(ref, x, y, x_dot_bounds, y_dot_bounds, mean_motion=0.001027, max_x_dot=10, max_y_dot=10,
+                              enforce_safety_constraints=True):
+    rel_x = x - ref.x
+    rel_y = y - ref.y
 
     # Velocity bound based on relative position
     # TODO: get rid of hardcoded values in constraint
-    constraint = 0.2 + 2 * mean_motion * math.sqrt(rel_x**2 + rel_y**2)
+    max_vel = 0.2 + 2 * mean_motion * math.sqrt(rel_x**2 + rel_y**2)
+    min_vel = -0.2 + 1/2 * mean_motion * math.sqrt(rel_x**2 + rel_y**2)
 
-    # Clamp bounds on max value
-    x_dot = set_max_bounds(x_dot, max_x_dot)
-    y_dot = set_max_bounds(y_dot, max_y_dot)
+    # Clamp bounds on max value and velcity constraint
+    if enforce_safety_constraints:
+        x_dot_bounds = set_max_bounds(set_max_bounds(x_dot_bounds, max_x_dot), max_vel)
+        y_dot_bounds = set_max_bounds(set_max_bounds(y_dot_bounds, max_y_dot), max_vel)
 
-    # Check constraint on largest possible velocity
-    max_abs_x_dot, max_abs_x_dot_idx = max_abs_value(x_dot)
-    max_abs_y_dot, max_abs_y_dot_idx = max_abs_value(y_dot)
-    while math.sqrt(max_abs_x_dot ** 2 + max_abs_y_dot ** 2) > constraint:
-        x_dot, y_dot = reduce_components(x_dot, y_dot, constraint)
-        max_abs_x_dot, max_abs_x_dot_idx = max_abs_value(x_dot)
-        max_abs_y_dot, max_abs_y_dot_idx = max_abs_value(y_dot)
+    # draw x_dot and y_dot values until constraint satisfied
+
+    draw_attempts = 0
+    constraint_satified = False
+    while not constraint_satified:
+        x_dot = draw_from_range(x_dot_bounds)
+        y_dot = draw_from_range(y_dot_bounds)
+        vel = math.sqrt(x_dot**2 + y_dot**2)
+        if (not enforce_safety_constraints) or (max_vel > vel and min_vel < vel):
+            constraint_satified = True
+
+        draw_attempts += 1
+        if draw_attempts >= 100:
+            raise ValueError("Cannot find initialization that meets safety constraints")
 
     return x_dot, y_dot
+
+
+def set_max_bounds(bounds, max_value):
+    if isinstance(bounds, list):
+        lower, upper = bounds[0], bounds[-1]
+        upper = min(max_value, upper)
+        lower = max(-max_value, lower)
+        bounds = [lower, upper]
+    else:
+        bounds = min(bounds, max_value) if bounds > 0 else max(bounds, -max_value)
+    return bounds
+
+
+def draw_from_range(bounds):
+    draw = bounds if type(bounds) != list else np.random.uniform(bounds[0], bounds[1])
+    return draw
