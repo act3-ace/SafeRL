1bd583f4144e51fd0cd85eec87e7e86f1b035976
JohnMcCarroll <jtm5356@g.rit.edu>
2021-07-29 11:03:44 -0400

    added extensible constraint testing package with test_docking module

---
+++ tests/system_tests/constants.py
@@ -0,0 +1,8 @@
+"""
+This module holds constant and default values for the system_tests package.
+"""
+
+# defaults
+DEFAULT_GPUS = 0
+DEFAULT_WORKERS = 6
+DEFAULT_FAKE_GPUS = False
---
+++ tests/system_tests/constraints/conftest.py
@@ -0,0 +1,31 @@
+"""
+This module defines common fixtures for the constraints testing package.
+
+Author: John McCarroll
+"""
+
+import pytest
+# from tests.system_tests.constraints.constants import DEFAULT_SEED
+DEFAULT_SEED = 0
+
+@pytest.fixture()
+def base_environment(config):
+    env = config["env"](config["env_config"])
+    return env
+
+
+@pytest.fixture()
+def modified_environment(base_environment):
+    # placeholder to enable extensible constraint testing setup
+    return base_environment
+
+
+@pytest.fixture()
+def step(modified_environment, action=None):
+    obs, reward, done, info = modified_environment.step(action)
+    return obs, reward, done, info
+
+
+@pytest.fixture()
+def seed():
+    return DEFAULT_SEED
