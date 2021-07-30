b32a8d934e3ed6272923761011bb09e3f5ae23a3
Umberto Ravaioli <uravaioli@toyon.com>
2021-07-30 16:39:57 -0700

    vel constraint reward uses generic proportional reward

--- saferl/aerospace/tasks/docking/processors.py
+++ saferl/aerospace/tasks/docking/processors.py
@@ -124,62 +124,6 @@ class DistanceChangeZRewardProcessor(RewardProcessor):
         return step_reward
 
 
-class ConditionalRewardProcessor(RewardProcessor):
-    def __init__(self, name, reward, cond_status):
-        self.cond_status = cond_status
-        self.last_step_size = 0
-        super().__init__(name, reward)
-
-    def reset(self, sim_state):
-        super().reset(sim_state)
-        self.last_step_size = 0
-
-    def _increment(self, sim_state, step_size):
-        self.last_step_size = step_size
-
-    def _process(self, sim_state):
-        cond = sim_state.status[self.cond_status]
-
-        if cond:
-            return self.reward
-        else:
-            return 0
-
-
-class ProportionalRewardProcessor(RewardProcessor):
-    def __init__(self, name, scale, bias, proportion_status, cond_status=None, cond_status_invert=False):
-        self.scale = scale
-        self.bias = bias
-        self.proportion_status = proportion_status
-        self.cond_status = cond_status
-        self.cond_status_invert = cond_status_invert
-        self.last_step_size = 0
-        super().__init__(name, reward=0)
-
-    def reset(self, sim_state):
-        super().reset(sim_state)
-        self.last_step_size = 0
-
-    def _increment(self, sim_state, step_size):
-        self.last_step_size = step_size
-
-    def _process(self, sim_state):
-        proportion = sim_state.status[self.proportion_status]
-        if self.cond_status is None:
-            cond = False
-        else:
-            cond = sim_state.status[self.cond_status]
-            if self.cond_status_invert:
-                cond = not cond
-
-        if cond:
-            reward = self.scale * proportion + self.bias
-        else:
-            reward = 0
-
-        return reward
-
-
 class SuccessRewardProcessor(RewardProcessor):
     def __init__(self, name=None, success_status=None, reward=None):
         super().__init__(name=name, reward=reward)
