0e353ba0726d6dbc51641bcdfcc953b1d2360fe2
JohnMcCarroll <jtm5356@g.rit.edu>
2021-07-31 11:56:08 -0400

    added velocity limit conformity tests and updated test values

--- tests/system_tests/constraints/test_docking.py
+++ tests/system_tests/constraints/test_docking.py
@@ -18,17 +18,17 @@ class TestVelocityConstraintExceedance:
     # testing exceedance of velocity limits
 
     test_states = [
-        [1500, 0, 3.1, 0],
-        [0, 1500, 0, 3.1],
-        [1060.6, 1060.6, 0, 3.1],
+        [1500, 0, 3.3, 0],
+        [0, 1500, 0, 3.3],
+        [1060.6, 1060.6, 2.4, 2.4],
 
-        [0.1, 0, 0.1, 0],
-        [0, 0.1, 0, 0.1],
-        [0.1, 0.1, 0.1, 0.1],
+        [0.21, 0, 0.1, 0],
+        [0, 0.21, 0, 0.1],
+        [0.15, 0.15, 0.075, 0.075],
 
-        [500, 0, 1.1, 0],
-        [0, 500, 0, 1.1],
-        [353.6, 353.6, 0.73, 0.73]
+        [500, 0, 1.25, 0],
+        [0, 500, 0, 1.25],
+        [353.6, 353.6, 0.89, 0.89]
     ]
 
     @pytest.fixture()
@@ -40,7 +40,6 @@ class TestVelocityConstraintExceedance:
         return "deputy"
 
     @pytest.mark.system_test
-    @pytest.mark.one
     @pytest.mark.parametrize("state", test_states, indirect=True)
     def test_velocity_constraint(self, step):
         # decompose the results of an environment step and assert the desired response from the environment.
@@ -49,23 +48,24 @@ class TestVelocityConstraintExceedance:
         assert info["status"]["max_vel_limit"] > 1
         assert info["reward"]["components"]["step"]["max_vel_constraint"] < 0
         assert done
+
 
 class TestVelocityConstraintConformity:
     # write one class per constraint under test
     # testing conforming to velocity limits
 
     test_states = [
-        [1500, 0, 2.9, 0],
-        [0, 1500, 0, 2.9],
-        [1060.6, 1060.6, 0, 3.1],
+        [1500, 0, 3.2, 0],
+        [0, 1500, 0, 3.2],
+        [1060.6, 1060.6, 2.3, 2.3],
 
-        [0.1, 0, 0.1, 0],
-        [0, 0.1, 0, 0.1],
-        [0.1, 0.1, 0.1, 0.1],
+        [0.21, 0, 0.09, 0],
+        [0, 0.21, 0, 0.09],
+        [0.15, 0.15, 0.07, 0.07],
 
-        [500, 0, 1.1, 0],
-        [0, 500, 0, 1.1],
-        [353.6, 353.6, 0.73, 0.73]
+        [500, 0, 1.2, 0],
+        [0, 500, 0, 1.2],
+        [353.6, 353.6, 0.84, 0.84]
     ]
 
     @pytest.fixture()
@@ -77,12 +77,16 @@ class TestVelocityConstraintConformity:
         return "deputy"
 
     @pytest.mark.system_test
-    @pytest.mark.one
     @pytest.mark.parametrize("state", test_states, indirect=True)
     def test_velocity_constraint(self, step):
         # decompose the results of an environment step and assert the desired response from the environment.
         obs, reward, done, info = step
-        assert info["status"]["max_vel_constraint"]
-        assert info["status"]["max_vel_limit"] > 1
-        assert info["reward"]["components"]["step"]["max_vel_constraint"] < 0
-        assert done
+        assert not info["status"]["max_vel_constraint"]
+        assert info["status"]["max_vel_limit"] < 1
+        assert info["reward"]["components"]["step"]["max_vel_constraint"] > 0
+        assert not done
+
+
+# TODO: test failure / success conditions IN docking region
+# TODO: can test values come from config? or always hard code...
+# TODO: file not found bug
