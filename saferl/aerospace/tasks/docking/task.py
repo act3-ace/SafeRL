86858eaebef0c4e43e6e4e7a929b315ee5702b45
james.cunningham <james.cunningham@jacobs.com>
2021-08-24 18:57:23 -0400

    Initial autoscaling

--- saferl/aerospace/tasks/docking/task.py
+++ saferl/aerospace/tasks/docking/task.py
@@ -8,6 +8,8 @@ class DockingEnv(BaseEnv):
         super().__init__(env_config)
         if self.renderer is None:
             self.renderer = DockingRender(**self.render_config)
+        docking_distance = self.sim_state.status["docking_distance"]
+        self.renderer.scale_factor = ((self.renderer.screen_width - 50) // 2) / docking_distance
 
     def reset(self):
         return super().reset()
