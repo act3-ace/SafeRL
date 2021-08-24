514fabaaf5e6d1ccc1fa3cd0a614e100c8f8e01c
JohnMcCarroll <jtm5356@g.rit.edu>
2021-08-24 11:10:46 -0400

    changed plane bodies to triangles and began safety_margin rendering work

--- saferl/aerospace/tasks/rejoin/render.py
+++ saferl/aerospace/tasks/rejoin/render.py
@@ -30,7 +30,8 @@ class RejoinRender:
                  y_threshold=10000,
                  scale_factor=25,
                  show_rejoin=True,
-                 show_ring=False,
+                 show_safety_ring=False,    # work in progress
+                 safety_margin=None,
                  plane_scale=1,
                  r_aircraft=15,
                  show_res=False,
@@ -44,10 +45,11 @@ class RejoinRender:
         self.plane_scale = plane_scale      # dialation of aircraft size
         self.scale_factor = scale_factor
         self.render_speed = render_speed
+        self.safety_margin = safety_margin
         self.viewer = None
 
         # Toggle shown items
-        self.show_ring = show_ring          # add ring around aircraft for visual aid
+        self.show_safety_ring = show_safety_ring          # add ring around aircraft for visual aid
         self.show_rejoin = show_rejoin      # render rejoin region for visual aid
         self.showRes = show_res
         self.termination_condition = termination_condition  # Set to true to print termination condition
@@ -65,11 +67,13 @@ class RejoinRender:
         screen_height = y_thresh * 2
         screen_width, screen_height = int(screen_width), int(screen_height)  # convert the screen width and height to integers
 
-        wingwidth = 25 * self.r_aircraft * self.plane_scale / self.scale_factor
-        wingheight = 5 * self.r_aircraft * self.plane_scale / self.scale_factor
+        wingwidth = 15 * self.r_aircraft * self.plane_scale / self.scale_factor
         bodywidth = 5 * self.r_aircraft * self.plane_scale / self.scale_factor
         bodyheight = 20 * self.r_aircraft * self.plane_scale / self.scale_factor
-        tailwidth = 10 * self.r_aircraft * self.plane_scale / self.scale_factor
+
+        if not self.safety_margin:
+            self.safety_margin = bodyheight / 2
+        safety_ring_r = self.safety_margin / self.scale_factor
 
         # process agent state
         wingman_state = state.env_objs["wingman"].state._vector
@@ -105,67 +109,49 @@ class RejoinRender:
 
             b, t, l, r = 0, self.y_threshold * 2, 0, self.x_threshold * 2  # + self.x_goal  # creates body dimensions
             sky = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])  # creates body polygon
-            # self.skytrans = rendering.Transform()  # allows body to be moved
-            # sky.add_attr(self.skytrans)
             sky.set_color(1, 1, 1)  # sets color of body
             self.viewer.add_geom(sky)  # adds body to viewer
 
-            # Create lead plane
-            b, t, l, r = -bodywidth / 2, bodywidth / 2, bodyheight / 2, -bodyheight / 2  # creates body dimensions
-            body = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])  # creates body polygon
-            self.bodytrans = rendering.Transform()  # allows body to be moved
-            body.add_attr(self.bodytrans)
-            body.set_color(.2, .2, .2)  # sets color of body
-            self.viewer.add_geom(body)  # adds body to viewer
-
-            b, t, l, r = -wingwidth / 2, wingwidth / 2, wingheight / 2, -wingheight / 2  # creates wing dimensions
-            wing = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])  # creates wing polygon
-            self.wingtrans = rendering.Transform()  # allows wing to be moved
-            wing.add_attr(self.wingtrans)
-            wing.add_attr(self.bodytrans)  # sets wing as part of body
-            wing.set_color(.3, .5, .3)  # sets color of wing
-            self.viewer.add_geom(wing)  # adds wing to viewer
-
-            b, t, l, r = -tailwidth / 2, tailwidth / 2, wingheight / 2, -wingheight / 2  # creates tail dimensions
-            tail = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])  # creates tail polygon
-            self.tailtrans = rendering.Transform(translation=(0, -bodyheight / 3))  # translates the tail to the end of the body
-            tail.add_attr(self.tailtrans)
-            tail.set_color(.3, .3, .5)  # sets color of tail
-            self.viewer.add_geom(tail)  # adds tail to render
+            # creates body dimensions
+            left_wing_x = -wingwidth / 2
+            right_wing_x = wingwidth / 2
+            wing_y = - bodyheight / 2
+            nose_x = 0
+            nose_y = bodyheight / 2
 
             # Create wingman plane
-            b, t, l, r = -bodywidth / 2, bodywidth / 2, bodyheight / 2, -bodyheight / 2  # creates body dimensions
-            body = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])  # creates body polygon
-            self.bodytrans_target = rendering.Transform()  # allows body to be moved
-            body.add_attr(self.bodytrans_target)
-            body.set_color(0, 0, 0)  # sets color of body
-            self.viewer.add_geom(body)  # adds body to viewer
+            wingman_body = rendering.FilledPolygon([(wing_y, left_wing_x), (wing_y, right_wing_x), (nose_y, nose_x)])
+            self.wingman_transform = rendering.Transform()  # allows body to be moved
+            wingman_body.add_attr(self.wingman_transform)
+            wingman_body.set_color(0.9, 0.0, 0.9)  # sets color of body
+            self.viewer.add_geom(wingman_body)  # adds body to viewer
 
-            b, t, l, r = -wingwidth / 2, wingwidth / 2, wingheight / 2, -wingheight / 2  # creates wing dimensions
-            wing = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])  # creates wing polygon
-            self.wingtrans_target = rendering.Transform()  # allows wing to be moved
-            wing.add_attr(self.wingtrans_target)
-            wing.add_attr(self.bodytrans_target)  # sets wing as part of body
-            wing.set_color(0, 0, 0)  # sets color of wing
-            self.viewer.add_geom(wing)  # adds wing to viewer
+            # Create lead plane
+            lead_body = rendering.FilledPolygon([(wing_y, left_wing_x), (wing_y, right_wing_x), (nose_y, nose_x)])
+            self.lead_transform = rendering.Transform()  # allows body to be moved
+            lead_body.add_attr(self.lead_transform)
+            lead_body.set_color(0, 0, 0)  # sets color of body
+            self.viewer.add_geom(lead_body)  # adds body to viewer
 
-            b, t, l, r = -tailwidth / 2, tailwidth / 2, wingheight / 2, -wingheight / 2  # creates tail dimensions
-            tail = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])  # creates tail polygon
-            self.tailtrans_target = rendering.Transform(translation=(0, -bodyheight / 3))  # translates the tail to the end of the body
-            tail.add_attr(self.tailtrans_target)
-            tail.set_color(0, 0, 0)  # sets color of tail
-            self.viewer.add_geom(tail)  # adds tail to render
+            if self.show_safety_ring:
+                # circle wingman
+                ring = rendering.make_circle(safety_ring_r, 20, False)  # creates ring dimensions
+                self.wingman_ring_transform = rendering.Transform()  # allows ring to be moved
+                ring.add_attr(self.wingman_ring_transform)
+                ring.add_attr(self.wingman_transform)  # sets ring as part of body
+                ring.set_color(.9, .0, .0)  # sets color of ring
+                self.viewer.add_geom(ring)  # adds ring into render
 
-            if self.show_ring:
-                ring = rendering.make_circle(wingwidth / 2, 30, False)  # creates ring dimensions
-                self.ringtrans = rendering.Transform()  # allows ring to be moved
-                ring.add_attr(self.ringtrans)
-                ring.add_attr(self.bodytrans)  # sets ring as part of body
+                # circle lead too
+                ring = rendering.make_circle(safety_ring_r, 20, False)  # creates ring dimensions
+                self.lead_ring_transform = rendering.Transform()  # allows ring to be moved
+                ring.add_attr(self.lead_ring_transform)
+                ring.add_attr(self.lead_transform)  # sets ring as part of body
                 ring.set_color(.9, .0, .0)  # sets color of ring
                 self.viewer.add_geom(ring)  # adds ring into render
 
             if self.show_rejoin:
-                ring = rendering.make_circle(rejoin_region_r, 30, False)  # creates ring dimensions
+                ring = rendering.make_circle(rejoin_region_r, 50, False)  # creates ring dimensions
                 self.rejoin_trans = rendering.Transform()  # allows ring to be moved
                 ring.add_attr(self.rejoin_trans)
                 ring.set_color(.9, .0, .0)  # sets color of ring
@@ -173,16 +159,12 @@ class RejoinRender:
                 self.viewer.add_geom(ring)  # adds ring into render
 
         # render agent plane
-        self.bodytrans.set_rotation(wingman_state[2])  # rotate body
-        self.bodytrans.set_translation(wingman_x, wingman_y)  # translate body
-        # self.tailtrans.set_rotation(wingman_state[2])  # rotate tail
-        # self.tailtrans.set_translation(wingman_x - wingman_trans_x, wingman_y - wingman_trans_y)  # translate tail
+        self.wingman_transform.set_rotation(wingman_state[2])  # rotate body
+        self.wingman_transform.set_translation(wingman_x, wingman_y)  # translate body
 
         # render lead plane
-        self.bodytrans_target.set_rotation(lead_state[2])  # rotate body
-        self.bodytrans_target.set_translation(lead_x, lead_y)  # translate body
-        # self.tailtrans_target.set_rotation(0)  # rotate tail
-        # self.tailtrans_target.set_translation(lead_x - lead_trans_x, lead_y + lead_trans_y)  # translate tail
+        self.lead_transform.set_rotation(lead_state[2])  # rotate body
+        self.lead_transform.set_translation(lead_x, lead_y)  # translate body
 
         # render rejoin region
         if self.show_rejoin:
--- saferl/aerospace/tasks/rejoin/task.py
+++ saferl/aerospace/tasks/rejoin/task.py
@@ -7,7 +7,13 @@ class DubinsRejoin(BaseEnv):
     def __init__(self, env_config):
         super().__init__(env_config)
         render_config = env_config["render"] if "render" in env_config.keys() else {}
-        self.renderer = RejoinRender(**render_config)
+        # get safety margin dims
+        safety_margin = None
+        for proc in self.status_manager.processors:
+            if hasattr(proc, "safety_margin"):
+                safety_margin = proc.safety_margin['aircraft']
+
+        self.renderer = RejoinRender(**render_config, safety_margin=safety_margin)
 
     def reset(self):
         return super().reset()
