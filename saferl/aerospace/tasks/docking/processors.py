ec3c76ff8218d84a2be9da7123d1188d6aeea915
JohnMcCarroll <jtm5356@g.rit.edu>
2021-09-02 15:20:15 -0400

    bug fixes

--- saferl/aerospace/tasks/docking/processors.py
+++ saferl/aerospace/tasks/docking/processors.py
@@ -10,26 +10,34 @@ from saferl.environment.models.geometry import distance
 
 class DockingObservationProcessor(ObservationProcessor):
     def __init__(self, name=None, deputy=None, mode='2d', normalization=None, clip=None, post_processors=None):
-        super().__init__(name=name, normalization=normalization, clip=clip, post_processors=post_processors)
         # Initialize member variables from config
-
         # 2d or 3d
         self.mode = mode
         # not platform ref, string for name of deputy
         self.deputy = deputy
+
+        # Invoke parent's constructor
+        super().__init__(name=name, normalization=normalization, clip=clip, post_processors=post_processors)
+
+        # add normalization + clipping
+        if not self.has_normalization:
+            if self.mode == '2d':
+                self.norm_const = np.array([1000, 1000, 10, 10])
+            elif self.mode == '3d':
+                self.norm_const = np.array([1000, 1000, 1000, 10, 10, 10])
 
     def define_observation_space(self) -> gym.spaces.Box:
         low = np.finfo(np.float32).min
         high = np.finfo(np.float32).max
 
         if self.mode == '2d':
-            self.observation_space = gym.spaces.Box(low=low, high=high, shape=(4,))
-            self.norm_const = np.array([1000, 1000, 10, 10])
+            observation_space = gym.spaces.Box(low=low, high=high, shape=(4,))
         elif self.mode == '3d':
-            self.observation_space = gym.spaces.Box(low=low, high=high, shape=(6,))
-            self.norm_const = np.array([1000, 1000, 1000, 10, 10, 10])
+            observation_space = gym.spaces.Box(low=low, high=high, shape=(6,))
         else:
             raise ValueError("Invalid observation mode {}. Should be '2d' or '3d'.".format(self.mode))
+
+        return observation_space
 
     def _process(self, sim_state):
         obs = sim_state.env_objs[self.deputy].state.vector
@@ -39,26 +47,30 @@ class DockingObservationProcessor(ObservationProcessor):
 
 class DockingObservationProcessorOriented(ObservationProcessor):
     def __init__(self, name=None, deputy=None, mode='2d', normalization=None, clip=None, post_processors=None):
-        # Invoke parent's constructor
-        super().__init__(name=name, normalization=normalization, clip=clip, post_processors=post_processors)
-
         # Initialize member variables from config
         self.mode = mode
         self.deputy = deputy
+
+        # Invoke parent's constructor
+        super().__init__(name=name, normalization=normalization, clip=clip, post_processors=post_processors)
+
+        if not self.has_normalization:
+            if self.mode == '2d':
+                # if no custom normalization defined
+                self._add_normalization([1000, 1000, np.pi, 100, 100, 0.4, 500])
 
     def define_observation_space(self) -> gym.spaces.Box:
         low = np.finfo(np.float32).min
         high = np.finfo(np.float32).max
 
         if self.mode == '2d':
-            self.observation_space = gym.spaces.Box(low=low, high=high, shape=(7,))
-            if not self.has_normalization:
-                # if no custom normalization defined
-                self._add_normalization([1000, 1000, np.pi, 100, 100, 0.4, 500])
+            observation_space = gym.spaces.Box(low=low, high=high, shape=(7,))
         elif self.mode == '3d':
             raise NotImplementedError
         else:
             raise ValueError("Invalid observation mode {}. Should be '2d' or '3d'.".format(self.mode))
+
+        return observation_space
 
     def _process(self, sim_state):
         obs = sim_state.env_objs[self.deputy].state.vector
