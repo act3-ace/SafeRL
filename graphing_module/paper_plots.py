2a0aaf4a5c2be7dafff882269a6ba959ec2ec6f3
vgangal <Vardaan.Gangal@Jacobs.com>
2021-09-30 11:17:32 -0400

    latest changes

--- graphing_module/paper_plots.py
+++ graphing_module/paper_plots.py
@@ -1,8 +1,19 @@
-
+import graphing_components
 # this file reproduces paper plots
 
-def paper_rejoin_plots()
-    return 
+def paper_rejoin_plots(logdir):
+    success_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','success_mean',int(1e5))
+    reward_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_reward_mean',int(1e5))
+    eps_length_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_length_mean',int(1e5))
+    return success_plot,reward_plot,eps_length_plot
 
-def paper_docking_plots()
-    return
+def paper_docking_plots(logdir):
+    success_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','success_mean',int(1e5))
+    reward_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_reward_mean',int(1e5))
+    eps_length_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_length_mean',int(1e5))
+    constr_viol_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','ratio_mean',int(1e5))
+    delta_v_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','delta_v_mean',int(1e5))
+    return success_plot,reward_plot,eps_length_plot,constr_viol_plot,delta_v_plot
+
+if __name__ == '__main__':
+    logdir =
