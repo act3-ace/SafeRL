008e471a58ca28d4f0ceea87f1f1d8bc461613c1
vgangal <Vardaan.Gangal@Jacobs.com>
2021-10-01 16:29:39 -0400

    modularized code, now running into overflow error and sns.relplot takes too long

--- graphing_module/graphing_components.py
+++ graphing_module/graphing_components.py
@@ -1,18 +1,14 @@
-import argparse
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import scipy
 import scipy.interpolate as interpolate
 
 def data_frame_processing(logdir):
     results_dir = logdir
     all_subdirs = next(os.walk(results_dir))[1]
 
-    # preprocessing logic
-
-    # keep track of all csv files
     csv_file_tracker = []
     for d in all_subdirs:
         if d == 'training_logs':
@@ -23,72 +19,80 @@ def data_frame_processing(logdir):
 
     data_dfs = [pd.read_csv(csv_file_tracker[i]) for i in range(len(csv_file_tracker))]
 
-    check_dir = all_subdirs[0]
-    check_dir_split = check_dir.lower().split("_")
-
-    rejoin_check = any('rejoin' in s for s in check_dir_split)
-    docking_check = any('docking' in s for s in check_dir_split)
-
-    exper_name = None
-    if rejoin_check == True:
-        exper_name = 'rejoin'
-    elif docking_check == True:
-        exper_name = 'docking'
-
-    return data_dfs, exper_name
+    return data_dfs
 
 
 def find_quantity_handle(quantity,data_dfs):
     look_quantity = quantity.lower()
     quantity_handle = None
+    col_names = list(data_dfs[0].columns)
+
     for label in col_names:
         if look_quantity in label:
             quantity_handle = label
 
-    if look_quantity == None:
+    if quantity_handle == None:
         print("Fatal: unable to find quantity in data files, please make sure the quantity is spelled correctly")
+        print('quantity name',quantity)
+        print(col_names)
         exit()
 
     return quantity_handle
 
 
 
-# how should clip_method be specified ?
 def clip(q1,data_dfs,clip_method):
-    # 3 cases , clip to upper bound , clip to shortest trajectory, specified trajectory
-    clipped_timesteps = None
+    """
+    Parameters:
+        q1 : str
+            name of quantity to be clipped
+        data_dfs : list of pandas dataframes
+            a list containing all the pandas dataframes that comprise the dataset
+        clip_method : str or int or tuple of ints
+            if 'string' , value must be 'short_traj' to specify clipping to the minimum bound of a quantity
+            if 'int' , quantity will be clipped will occur from (0,clip_method+1) ,
+            If a tuple of ints is passed quantity will be clipped into following range (lower_bound,upper_bound)
+
+    Returns
+        clipped_quantity: list
+            list of ints over the specified clipping bounds for a quantity
+
+    """
+
+    clipped_quantity = None
     # need q1 handle
     q1_handle = find_quantity_handle(q1,data_dfs)
 
     if clip_method == 'short_traj':
         check_pos = data_dfs[0].shape[0] -1
-        per_df_max_timesteps = [ds.iloc[[check_pos]][q1_handle] for ds in data_dfs]
+        per_df_max_vals = [ds.iloc[[check_pos]][q1_handle] for ds in data_dfs]
         min_df_pos = np.argmin(per_df_max_vals)
 
         check_max_pos_row = data_dfs[min_df_pos].shape[0]-1
         q1_min = data_dfs[min_df_pos].iloc[[check_max_pos_row]][q1_handle]
         clipped_timesteps = np.array(list(range(q1_min)))
-    # needs its own argument
     elif type(clip_method) == int:
         # clip to an upper bound
         upper_bound = clip_method
-        clipped_timesteps = np.array(list(range(int(upper_bound)+1)))
+        clipped_quantity = np.array(list(range(int(upper_bound)+1)))
     elif type(clip_method) == tuple:
+        # clip between bounds
         bounds = clip_method
         lower_bound, upper_bound = bounds
-        clipped_timesteps = np.array(list(range(lower, upper_bound+1)))
+        clipped_quantity = np.array(list(range(lower, upper_bound+1)))
 
-    return clipped_timesteps
+    return clipped_quantity
 
 
 def plot_quantity1_v_quantity2(data_dfs,q1,q2,clip_method):
+    # need to look through df.columns to get appropriate handle
     q1_handle = find_quantity_handle(q1,data_dfs)
     q2_handle = find_quantity_handle(q2,data_dfs)
-    # need to look through df.columns to get appropriate handle
+
     interp = False
     if clip_method is not None:
         interp = True
-        clipped_q1 = clip(clip_method,data_dfs,q1)
+        q1_clipped = clip(q1,data_dfs,clip_method)
 
 
     q1_track = []
@@ -97,12 +101,12 @@ def plot_quantity1_v_quantity2(data_dfs,q1,q2,clip_method):
 
     for ds in data_dfs:
         q1_value = ds[q1_handle]
-        q2_value = ds[quantity_handle]
+        q2_value = ds[q2_handle]
 
         if interp:
             func_q1_v_q2 = interpolate.interp1d(q1_value,q2_value,fill_value='extrapolate')
 
-            q2_value = func_time_v_quantity(clipped_q1)
+            q2_value = func_q1_v_q2(q1_clipped)
 
             for i in q1_clipped:
                 q1_track.append(i)
@@ -117,6 +121,7 @@ def plot_quantity1_v_quantity2(data_dfs,q1,q2,clip_method):
     graph_data[q1] = q1_track
     graph_data[q2] = q2_track
 
+    sns.set_theme()
     plot = sns.relplot(data=graph_data,x=q1,y=q2,kind='line')
     plot.set_axis_labels(q1,q2)
 
@@ -126,11 +131,8 @@ def plot_quantity1_v_quantity2(data_dfs,q1,q2,clip_method):
 
     return plot
 
-# general graph function
-# needs logdir
-# set of quantities
-# pass clipping method
+# graph wrapper function
 def graph_q1_v_q2(logdir,q1,q2,clip_method):
-    data_dfs,exper_name = data_frame_processing(logdir)
+    data_dfs = data_frame_processing(logdir)
     plot_handle = plot_quantity1_v_quantity2(data_dfs,q1,q2,clip_method)
     return plot_handle
--- graphing_module/paper_plots.py
+++ graphing_module/paper_plots.py
@@ -1,19 +1,22 @@
 import graphing_components
 # this file reproduces paper plots
 
-def paper_rejoin_plots(logdir):
-    success_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','success_mean',int(1e5))
-    reward_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_reward_mean',int(1e5))
-    eps_length_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_length_mean',int(1e5))
+def paper_rejoin_plots(logdir,clip_method):
+    #success_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','success_mean',clip_method)
+    #reward_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_reward_mean',clip_method)
+    eps_length_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_len_mean',clip_method)
     return success_plot,reward_plot,eps_length_plot
 
-def paper_docking_plots(logdir):
-    success_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','success_mean',int(1e5))
-    reward_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_reward_mean',int(1e5))
-    eps_length_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_length_mean',int(1e5))
-    constr_viol_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','ratio_mean',int(1e5))
-    delta_v_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','delta_v_mean',int(1e5))
+def paper_docking_plots(logdir,clip_method):
+    success_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','success_mean',clip_method)
+    reward_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_reward_mean',clip_method)
+    eps_length_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','episode_length_mean',clip_method)
+    constr_viol_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','ratio_mean',clip_method)
+    delta_v_plot = graphing_components.graph_q1_v_q2(logdir,'timesteps_total','delta_v_mean',clip_method)
     return success_plot,reward_plot,eps_length_plot,constr_viol_plot,delta_v_plot
 
 if __name__ == '__main__':
-    logdir =
+    #docking_logdir = '/home/vgangal/Downloads/expr_20210914_005226'
+    rejoin_logdir = '/home/vgangal/Downloads/expr_20210909_194211'
+    paper_rejoin_plots(rejoin_logdir,int(5.5e5))
+    #paper_docking_plots(docking_logdir,int(1e5))
