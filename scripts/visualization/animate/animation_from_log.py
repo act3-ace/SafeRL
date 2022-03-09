a4b3479ebbb6c4bac27587bec7a0e3a8f9a03dfb
Umberto Ravaioli <uravaioli@toyon.com>
2022-03-09 13:33:24 -0800

    animations/figures for presentation

--- scripts/visualization/animate/animation_from_log.py
+++ scripts/visualization/animate/animation_from_log.py
@@ -24,6 +24,8 @@ def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('log', type=str, help="The full path to the trajectory log file")
+    parser.add_argument('animator', type=str, help="docking_2d, docking_2d_oriented")
+    parser.add_argument('--output', type=str, default="anim.mp4")
 
     return parser.parse_args()
 
@@ -32,11 +34,32 @@ class animator(abc.ABC):
     def __init__(self, log_data, concurrent=False):
         self.log_data = log_data
         self.concurrent = concurrent
+        self.ui_scale_mat = np.eye(2)
 
         if self.concurrent:
             self.num_frames = max([len(d) for d in self.log_data])
         else:
             self.num_frames = sum([len(d) for d in self.log_data])
+
+    def animate(self):
+        self.fig, self.ax = plt.subplots(subplot_kw={'adjustable': 'box', 'aspect': 'equal'})
+        self.ax.set_xlim(left=self.xlim[0], right=self.xlim[1])
+        self.ax.set_ylim(bottom=self.ylim[0], top=self.ylim[1])
+
+        xlim_span = self.xlim[1] - self.xlim[0]
+        ylim_span = self.ylim[1] - self.ylim[0]
+
+        self.ui_scale_mat = np.array([[max(xlim_span, ylim_span), 0], [0, max(xlim_span, ylim_span)]], dtype=float)
+
+        self.setup_artists()
+
+        anim = animation.FuncAnimation(
+            self.fig, self.frame_change, frames=self.num_frames, init_func=self.frame_init, blit=True, interval=16.7)
+
+        return anim
+
+    def setup_artists(self):
+        pass
 
     def get_sequential_index(self, frame):
 
@@ -49,20 +72,74 @@ class animator(abc.ABC):
 
         return i, frame
 
-    @abc.abstractmethod
-    def animate(self):
-        raise NotImplementedError
-
-    @abc.abstractmethod
     def frame_init(self):
-        raise NotImplementedError
+        return []
 
     @abc.abstractmethod
     def frame_change(self, frame):
         raise NotImplementedError
 
 
-class animator_docking_oriented_2d(animator):
+class animator_docking_2d(animator):
+    def __init__(self, log_data, concurrent=False):
+        super().__init__(log_data, concurrent=concurrent)
+
+        deputy_xs = [d['info']['deputy']['x'] for ep_log in self.log_data for d in ep_log]
+        deputy_ys = [d['info']['deputy']['y'] for ep_log in self.log_data for d in ep_log]
+
+        self.xlim = (min(deputy_xs+[0]), max(deputy_xs+[0]))
+        self.ylim = (min(deputy_ys+[0]), max(deputy_ys+[0]))
+
+        xlim_buffer = 0.1 * (self.xlim[1] - self.xlim[0])
+        ylim_buffer = 0.1 * (self.ylim[1] - self.ylim[0])
+
+        self.xlim = (self.xlim[0] - xlim_buffer, self.xlim[1] + xlim_buffer)
+        self.ylim = (self.ylim[0] - ylim_buffer, self.ylim[1] + ylim_buffer)
+
+    def setup_artists(self):
+        self.deputy_patch = patches.Polygon(get_square_verts(0, 0, scale=0.1*self.ui_scale_mat).T)
+        self.ax.add_patch(self.deputy_patch)
+
+        self.deputy_thruster_x = patches.Polygon(thrust_wedge_verts.copy().T, fc='r')
+        self.ax.add_patch(self.deputy_thruster_x)
+
+        self.deputy_thruster_y = patches.Polygon(thrust_wedge_verts.copy().T, fc='r')
+        self.ax.add_patch(self.deputy_thruster_y)
+
+    def frame_change(self, frame):
+         # return self._frame_change(self.log_data[frame])
+        artists = []
+
+        if not self.concurrent:
+            ep_idx, ts_idx = self.get_sequential_index(frame)
+            data = self.log_data[ep_idx][ts_idx]
+        else:
+            data = None
+
+        x = data['info']['deputy']['x']
+        y = data['info']['deputy']['y']
+        control = data['info']['deputy']['controller']['control']
+        thrust_x = control[0]
+        thrust_y = control[1]
+
+        # import pdb; pdb.set_trace()
+        deputy_verts = get_square_verts(x, y, scale=0.1*self.ui_scale_mat)
+        self.deputy_patch.set_xy(deputy_verts.T)
+        artists.append(self.deputy_patch)
+
+        thruster_verts = rotate_scale_transform_verts(thrust_wedge_verts.copy(), x, y, 0,
+                                                      scale=0.2*thrust_x*self.ui_scale_mat)
+        self.deputy_thruster_x.set_xy(thruster_verts.T)
+        artists.append(self.deputy_thruster_x)
+
+        thruster_verts = rotate_scale_transform_verts(thrust_wedge_verts.copy(), x, y, 0,
+                                                      scale=0.2*thrust_y*self.ui_scale_mat)
+        self.deputy_thruster_y.set_xy(thruster_verts.T)
+        artists.append(self.deputy_thruster_y)
+
+        return artists
+
+class animator_docking_2d_oriented(animator):
 
     def __init__(self, log_data):
         super().__init__(log_data, concurrent=False)
@@ -79,33 +156,23 @@ class animator_docking_oriented_2d(animator):
         self.xlim = (self.xlim[0] - xlim_buffer, self.xlim[1] + xlim_buffer)
         self.ylim = (self.ylim[0] - ylim_buffer, self.ylim[1] + ylim_buffer)
 
-        self.ui_scale_mat = np.eye(2)
-
-    def animate(self):
-        self.fig, self.ax = plt.subplots(subplot_kw={'adjustable': 'box', 'aspect': 'equal'})
-        self.ax.set_xlim(left=self.xlim[0], right=self.xlim[1])
-        self.ax.set_ylim(bottom=self.ylim[0], top=self.ylim[1])
-
-        xlim_span = self.xlim[1] - self.xlim[0]
-        ylim_span = self.ylim[1] - self.ylim[0]
-
-        self.ui_scale_mat = np.array([[max(xlim_span, ylim_span), 0], [0, max(xlim_span, ylim_span)]], dtype=float)
-
-        anim = animation.FuncAnimation(
-            self.fig, self.frame_change, frames=self.num_frames, init_func=self.frame_init, blit=True, interval=16.7)
-
-        return anim
-
-    def frame_init(self):
+    def setup_artists(self):
         artists = []
+        self.origin_patch = patches.Circle((0, 0), radius=10, ec='g', fc='palegreen')
+        self.ax.add_patch(self.origin_patch)
+        artists.append(self.origin_patch)
 
-        self.deputy_patch = patches.Polygon(get_chevron_verts(0, 0).T)
+        self.deputy_patch = patches.Polygon(get_chevron_verts(0, 0, scale=0.1*self.ui_scale_mat).T, fc='tab:blue')
         self.ax.add_patch(self.deputy_patch)
         artists.append(self.deputy_patch)
 
-        self.deputy_thruster = patches.Polygon(thrust_wedge_verts.copy().T, fc='r')
+        self.deputy_thruster = patches.Polygon(thrust_wedge_verts.copy().T, fc='tab:red')
         self.ax.add_patch(self.deputy_thruster)
         artists.append(self.deputy_thruster)
+
+        self.chief_patch = patches.Circle((0, 0), radius=0.5, fc='g')
+        self.ax.add_patch(self.chief_patch)
+        artists.append(self.chief_patch)
 
         return artists
 
@@ -165,9 +232,21 @@ thrust_wedge_verts = np.array([
     [-1, -0.26],
 ]).T
 
+square_verts = np.array([
+    [-0.5, -0.5],
+    [0.5, -0.5],
+    [0.5, 0.5],
+    [-0.5, 0.5],
+]).T
+
 
 def get_chevron_verts(x, y, theta=0, scale=1):
     verts = chevron_verts.copy()
+    return rotate_scale_transform_verts(verts, x, y, theta, scale)
+
+
+def get_square_verts(x, y, theta=0, scale=1):
+    verts = square_verts.copy()
     return rotate_scale_transform_verts(verts, x, y, theta, scale)
 
 
@@ -189,21 +268,27 @@ def get_rot_mat_2d(theta):
     ], dtype=float)
 
 
+animator_map = {
+    'docking_2d': animator_docking_2d,
+    'docking_2d_oriented': animator_docking_2d_oriented,
+}
+
+
 def main():
     # process args
     args = get_args()
     log_data = parse_jsonlines_log(args.log, separate_episodes=True)
 
-    # log_data = log_data[0:100]
+    # log_data = [log_data[0][0:100]]
 
     print(len(log_data))
-    animator = animator_docking_oriented_2d(log_data)
+    animator = animator_map[args.animator](log_data)
 
     anim = animator.animate()
     print("saving animation")
 
     save_bar = tqdm(total=animator.num_frames)
-    anim.save('anim.mp4', writer="ffmpeg", dpi=200,
+    anim.save(args.output, writer="ffmpeg", dpi=200,
               progress_callback=lambda i, n: save_bar.update(1))
     save_bar.close()
     # plt.show()
