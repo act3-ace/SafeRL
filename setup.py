0c3df31039afa5be38ba555f60ca72d757adea33
Umberto Ravaioli <uravaioli@toyon.com>
2022-06-02 18:48:37 -0700

    updated reqs for python 3.8

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setuptools.setup(
         # "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7, <3.9',
+    python_requires='>=3.8',
     install_requires=reqs,
 )
